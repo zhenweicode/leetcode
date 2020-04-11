1929d9e8f0d125b9ab7edf08df1a363789abee09
xiezhenwei <xiezhenwei@meituan.com>
2020-04-11 13:39:04 +0800

    push

--- 2.题目/Solution.java
+++ 2.题目/Solution.java
@@ -26,17 +26,17 @@ class Solution {
 }
 
 class Solution {
-    public int jump(int[] nums) {
-        int end = 0;
-        int maxPosition = 0;
-        int steps = 0;
-        for (int i = 0; i < nums.length - 1; i++) {
-            maxPosition = Math.max(maxPosition, nums[i] + i);   //找能跳的最远的
-            if (i == end) { //遇到边界，就更新边界，并且步数加一
-                end = maxPosition;
-                steps++;
-            }
+    HashSet<Integer> set = new HashSet<>();
+
+    public boolean canReach(int[] arr, int start) {
+        if (start < 0 || start >= arr.length) {   // 判断下标是否合法
+            return false;
         }
-        return steps;
+        if (arr[start] == 0) {         // 找到target 返回true
+            return true;
+        } else if (!set.add(start)) {
+            return false;            //表明当前位置已访问过，直接返回false
+        }
+        return canReach(arr, start + arr[start]) || canReach(arr, start - arr[start]);
     }
 }
