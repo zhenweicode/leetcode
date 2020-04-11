0a387e4ff0304d6e7c4acaf7551ac3293eb88432
xiezhenwei <xiezhenwei@meituan.com>
2020-04-11 13:26:14 +0800

    push

--- 2.题目/Solution.java
+++ 2.题目/Solution.java
@@ -24,3 +24,19 @@ class Solution {
         return ans;
     }
 }
+
+class Solution {
+    public int jump(int[] nums) {
+        int end = 0;
+        int maxPosition = 0;
+        int steps = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            maxPosition = Math.max(maxPosition, nums[i] + i);   //找能跳的最远的
+            if (i == end) { //遇到边界，就更新边界，并且步数加一
+                end = maxPosition;
+                steps++;
+            }
+        }
+        return steps;
+    }
+}
