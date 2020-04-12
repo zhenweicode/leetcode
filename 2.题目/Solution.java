307f779c523d0151c190bf4379836dda1a6da8de
xiezhenwei <xiezhenwei@meituan.com>
2020-04-12 14:45:54 +0800

    push

--- 2.题目/Solution.java
+++ 2.题目/Solution.java
@@ -26,33 +26,27 @@ class Solution {
 }
 
 class Solution {
-    public static int[] cal_next(String str) {                // 计算关于 模式串 的 next 数组
-        int len = str.length(), next[] = new int[len];
-        next[0] = -1;
-        int i = 0, j = 1;                                    // i j 分别是前后缀指针
-        while (j < len) {
-            if (i == -1 || str.charAt(i) == str.charAt(j)) {
-                i++;
-                j++;
-                if (j < len) next[j] = i;
-            } else i = next[i];
+    public static int solution2(int nLevel, int kChess) {
+        if (nLevel < 1 || kChess < 1) {
+            return 0;
         }
-
-        return next;
-    }
-
-    public static int strStr(String haystack, String needle) {
-        if (needle.length() == 0) return 0;
-        if (haystack.length() == 0) return -1;
-        int[] next = cal_next(needle);                        // 计算 needle 的前后缀
-        int i = 0, j = 0, len = haystack.length(), subLen = needle.length();
-        while (i < len) {
-            if (j == -1 || haystack.charAt(i) == needle.charAt(j)) {
-                i++;
-                j++;
-                if (j == subLen) return i - j;                // needle 指针走到末尾，说明匹配成功
-            } else j = next[j];
+        if (kChess == 1) {
+            return nLevel;
         }
-        return -1;
+        int[][] dp = new int[nLevel + 1][kChess + 1];
+        for (int i = 1; i != dp.length; i++) {
+            dp[i][1] = i;
+        }
+        for (int i = 1; i != dp.length; i++) {
+            for (int j = 2; j != dp[0].length; j++) {
+                int min = Integer.MAX_VALUE;
+                for (int k = 1; k != i + 1; k++) {
+                    min = Math.min(min,
+                            Math.max(dp[k - 1][j - 1], dp[i - k][j]));
+                }
+                dp[i][j] = min + 1;
+            }
+        }
+        return dp[nLevel][kChess];
     }
 }
