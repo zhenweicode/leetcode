14d5008f3cea1f0cf96e72317fc3cfe65f93115c
xiezhenwei <xiezhenwei@meituan.com>
2020-04-12 14:28:07 +0800

    push

--- 2.题目/Solution.java
+++ 2.题目/Solution.java
@@ -25,56 +25,34 @@ class Solution {
     }
 }
 
-public class Solution {
-
-    public int superEggDrop(int K, int N) {
-        // dp[i][j]：一共有 i 层楼梯的情况下，使用 j 个鸡蛋的最少仍的次数
-        int[][] dp = new int[N + 1][K + 1];
-
-        // 初始化
-        for (int i = 0; i <= N; i++) {
-            dp[i] = i;
-        }
-        for (int j = 0; j <= K; j++) {
-            dp[0][j] = 0;
-        }
-
-        dp[1][0] = 0;
-        for (int j = 1; j <= K; j++) {
-            dp[1][j] = 1;
-        }
-        for (int i = 0; i <= N; i++) {
-            dp[i][0] = 0;
-            dp[i][1] = i;
+class Solution {
+    public static int[] cal_next(String str) {                // 计算关于 模式串 的 next 数组
+        int len = str.length(), next[] = new int[len];
+        next[0] = -1;
+        int i = 0, j = 1;                                    // i j 分别是前后缀指针
+        while (j < len) {
+            if (i == -1 || str.charAt(i) == str.charAt(j)) {
+                i++;
+                j++;
+                if (j < len) next[j] = i;
+            } else i = next[i];
         }
 
-        // 开始递推
-        for (int i = 2; i <= N; i++) {
-            for (int j = 2; j <= K; j++) {
-                // 在区间 [1, i] 里确定一个最优值
-                int left = 1;
-                int right = i;
-                while (left < right) {
-                    // 找 dp[k - 1][j - 1] <= dp[i - mid][j] 的最大值 k
-                    int mid = left + (right - left + 1) / 2;
+        return next;
+    }
 
-                    int breakCount = dp[mid - 1][j - 1];
-                    int notBreakCount = dp[i - mid][j];
-                    if (breakCount > notBreakCount) {
-                        // 排除法（减治思想）写对二分见第 35 题，先想什么时候不是解
-                        // 严格大于的时候一定不是解，此时 mid 一定不是解
-                        // 下一轮搜索区间是 [left, mid - 1]
-                        right = mid - 1;
-                    } else {
-                        // 这个区间一定是上一个区间的反面，即 [mid, right]
-                        // 注意这个时候取中间数要上取整，int mid = left + (right - left + 1) / 2;
-                        left = mid;
-                    }
-                }
-                // left 这个下标就是最优的 k 值，把它代入转移方程 Math.max(dp[k - 1][j - 1], dp[i - k][j]) + 1) 即可
-                dp[i][j] = Math.max(dp[left - 1][j - 1], dp[i - left][j]) + 1;
-            }
+    public static int strStr(String haystack, String needle) {
+        if (needle.length() == 0) return 0;
+        if (haystack.length() == 0) return -1;
+        int[] next = cal_next(needle);                        // 计算 needle 的前后缀
+        int i = 0, j = 0, len = haystack.length(), subLen = needle.length();
+        while (i < len) {
+            if (j == -1 || haystack.charAt(i) == needle.charAt(j)) {
+                i++;
+                j++;
+                if (j == subLen) return i - j;                // needle 指针走到末尾，说明匹配成功
+            } else j = next[j];
         }
-        return dp[N][K];
+        return -1;
     }
 }
