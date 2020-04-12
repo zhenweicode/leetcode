b607ea87ce68b87b7f531e77478b77ca82ff4275
xiezhenwei <xiezhenwei@meituan.com>
2020-04-12 15:45:11 +0800

    init

--- 2.题目/Solution.java
+++ 2.题目/Solution.java
@@ -26,27 +26,22 @@ class Solution {
 }
 
 class Solution {
-    public static int solution2(int nLevel, int kChess) {
-        if (nLevel < 1 || kChess < 1) {
-            return 0;
+    int res, k;
+
+    public int kthLargest(TreeNode root, int k) {
+        this.k = k;
+        dfs(root);
+        return res;
+    }
+
+    void dfs(TreeNode root) {
+        if (root == null) return;
+        dfs(root.right);
+
+        if (--k == 0) {
+            res = root.val;
+            return;
         }
-        if (kChess == 1) {
-            return nLevel;
-        }
-        int[][] dp = new int[nLevel + 1][kChess + 1];
-        for (int i = 1; i != dp.length; i++) {
-            dp[i][1] = i;
-        }
-        for (int i = 1; i != dp.length; i++) {
-            for (int j = 2; j != dp[0].length; j++) {
-                int min = Integer.MAX_VALUE;
-                for (int k = 1; k != i + 1; k++) {
-                    min = Math.min(min,
-                            Math.max(dp[k - 1][j - 1], dp[i - k][j]));
-                }
-                dp[i][j] = min + 1;
-            }
-        }
-        return dp[nLevel][kChess];
+        dfs(root.left);
     }
 }
