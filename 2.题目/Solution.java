d014cf1ee64e9a5c2ce63a91efbc0e83e0df8f92
xiezhenwei <xiezhenwei@meituan.com>
2020-04-13 23:55:24 +0800

    push

--- 2.题目/Solution.java
+++ 2.题目/Solution.java
@@ -26,22 +26,30 @@ class Solution {
 }
 
 class Solution {
-    int res, k;
-
-    public int kthLargest(TreeNode root, int k) {
-        this.k = k;
-        dfs(root);
-        return res;
+    public int numDecodings(String s) {
+        return getAns(s, 0);
     }
 
-    void dfs(TreeNode root) {
-        if (root == null) return;
-        dfs(root.right);
-
-        if (--k == 0) {
-            res = root.val;
-            return;
+    private int getAns(String s, int start) {
+        //划分到了最后返回 1
+        if (start == s.length()) {
+            return 1;
         }
-        dfs(root.left);
+        //开头是 0,0 不对应任何字母，直接返回 0
+        if (s.charAt(start) == '0') {
+            return 0;
+        }
+        //得到第一种的划分的解码方式
+        int ans1 = getAns(s, start + 1);
+        int ans2 = 0;
+        //判断前两个数字是不是小于等于 26 的
+        if (start < s.length() - 1) {
+            int ten = (s.charAt(start) - '0') * 10;
+            int one = s.charAt(start + 1) - '0';
+            if (ten + one <= 26) {
+                ans2 = getAns(s, start + 2);
+            }
+        }
+        return ans1 + ans2;
     }
 }
