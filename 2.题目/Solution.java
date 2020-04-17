974fe4b04cb981eac4fb313fdbf79d3e073b90fe
xiezhenwei <xiezhenwei@meituan.com>
2020-04-17 11:40:58 +0800

    push

--- 2.题目/Solution.java
+++ 2.题目/Solution.java
@@ -1,30 +1,26 @@
 class Solution {
-    public int trap(int[] height) {
-        int sum = 0;
-        int max_left = 0;
-        int max_right = 0;
-        int left = 1;
-        int right = height.length - 2; // 加右指针进去
-        for (int i = 1; i < height.length - 1; i++) {
-            //从左到右更
-            if (height[left - 1] < height[right + 1]) {
-                max_left = Math.max(max_left, height[left - 1]);
-                int min = max_left;
-                if (min > height[left]) {
-                    sum = sum + (min - height[left]);
-                }
-                left++;
-                //从右到左更
-            } else {
-                max_right = Math.max(max_right, height[right + 1]);
-                int min = max_right;
-                if (min > height[right]) {
-                    sum = sum + (min - height[right]);
-                }
-                right--;
+    public static List<List<Integer>> threeSum(int[] nums) {
+        if (nums == null || nums.length < 3) return new ArrayList<>();
+        List<List<Integer>> ans = new ArrayList();
+        int len = nums.length;
+        Arrays.sort(nums); // 排序
+        for (int i = 0; i < len; i++) {
+            if (nums[i] > 0) break; // 如果当前数字大于0，则三数之和一定大于0，所以结束循环
+            if (i > 0 && nums[i] == nums[i - 1]) continue; // 去重
+            int L = i + 1;
+            int R = len - 1;
+            while (L < R) {
+                int sum = nums[i] + nums[L] + nums[R];
+                if (sum == 0) {
+                    ans.add(Arrays.asList(nums[i], nums[L], nums[R]));
+                    while (L < R && nums[L] == nums[L + 1]) L++; // 去重
+                    while (L < R && nums[R] == nums[R - 1]) R--; // 去重
+                    L++;
+                    R--;
+                } else if (sum < 0) L++;
+                else if (sum > 0) R--;
             }
         }
-        return sum;
+        return ans;
     }
-
 }
