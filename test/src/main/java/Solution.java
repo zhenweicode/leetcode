1288a3755187609c3e6688fead9ed6081f31853d
xiezhenwei <xiezhenwei@meituan.com>
2020-04-17 23:06:30 +0800

    push

--- test/src/main/java/Solution.java
+++ test/src/main/java/Solution.java
@@ -3,4 +3,15 @@ import java.util.Collections;
 import java.util.List;
 
 class Solution {
+    public List<Integer> grayCode(int n) {
+        List<Integer> res = new ArrayList<>();
+        res.add(0);
+        int head = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = res.size() - 1; j >= 0; j--)
+                res.add(head + res.get(j));
+            head <<= 1;
+        }
+        return res;
+    }
 }
