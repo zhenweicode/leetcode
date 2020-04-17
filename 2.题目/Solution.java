c8f3ce2efad86edd7382b64fe983d1b60ac64541
xiezhenwei <xiezhenwei@meituan.com>
2020-04-17 11:52:19 +0800

    push

--- 2.题目/Solution.java
+++ 2.题目/Solution.java
@@ -1,26 +1,15 @@
 class Solution {
-    public static List<List<Integer>> threeSum(int[] nums) {
-        if (nums == null || nums.length < 3) return new ArrayList<>();
-        List<List<Integer>> ans = new ArrayList();
-        int len = nums.length;
-        Arrays.sort(nums); // 排序
-        for (int i = 0; i < len; i++) {
-            if (nums[i] > 0) break; // 如果当前数字大于0，则三数之和一定大于0，所以结束循环
-            if (i > 0 && nums[i] == nums[i - 1]) continue; // 去重
-            int L = i + 1;
-            int R = len - 1;
-            while (L < R) {
-                int sum = nums[i] + nums[L] + nums[R];
-                if (sum == 0) {
-                    ans.add(Arrays.asList(nums[i], nums[L], nums[R]));
-                    while (L < R && nums[L] == nums[L + 1]) L++; // 去重
-                    while (L < R && nums[R] == nums[R - 1]) R--; // 去重
-                    L++;
-                    R--;
-                } else if (sum < 0) L++;
-                else if (sum > 0) R--;
+    public boolean canAttendMeetings(int[][] intervals) {
+        Arrays.sort(intervals, new Comparator<int[]>() {
+            public int compare(int[] i1, int[] i2) {
+                return i1[0] - i2[0];
             }
+        });
+
+        for (int i = 0; i < intervals.length - 1; i++) {
+            if (intervals[i][1] > intervals[i + 1][0])
+                return false;
         }
-        return ans;
+        return true;
     }
 }
