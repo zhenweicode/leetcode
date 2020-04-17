60c0e543a96ffecd6a7e4e88f57c7283ce1f56fd
xiezhenwei <xiezhenwei@meituan.com>
2020-04-17 09:44:12 +0800

    push

--- 2.题目/Solution.java
+++ 2.题目/Solution.java
@@ -1,24 +1,19 @@
 class Solution {
-    public static int getMaxLength(int[] arr, int k) {
-        if (arr == null || arr.length == 0 || k <= 0) {
+    public static int maxLength(int[] arr, int k) {
+        if (arr == null || arr.length == 0) {
             return 0;
         }
-        int left = 0;
-        int right = 0;
-        int sum = arr[0];
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        map.put(0, -1); // important
         int len = 0;
-        while (right < arr.length) {
-            if (sum == k) {
-                len = Math.max(len, right - left + 1);
-                sum -= arr[left++];
-            } else if (sum < k) {
-                right++;
-                if (right == arr.length) {
-                    break;
-                }
-                sum += arr[right];
-            } else {
-                sum -= arr[left++];
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+            if (map.containsKey(sum - k)) {
+                len = Math.max(i - map.get(sum - k), len);
+            }
+            if (!map.containsKey(sum)) {
+                map.put(sum, i);
             }
         }
         return len;
