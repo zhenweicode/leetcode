cffbd707e6417c8456abb5b1cf8bffca8b71c36d
xiezhenwei <xiezhenwei@meituan.com>
2020-04-17 11:16:11 +0800

    push

--- 2.题目/Solution.java
+++ 2.题目/Solution.java
@@ -1,39 +1,30 @@
 class Solution {
-    public static void splitArray(int[] arr, int k) {
-        if (arr == null || arr.length < 2) {
-            return;
-        }
-
-        int left = 0;
-        int right = arr.length - 1;
-        while (left < right) {
-            while (left < right && arr[left] < k) {
+    public int trap(int[] height) {
+        int sum = 0;
+        int max_left = 0;
+        int max_right = 0;
+        int left = 1;
+        int right = height.length - 2; // 加右指针进去
+        for (int i = 1; i < height.length - 1; i++) {
+            //从左到右更
+            if (height[left - 1] < height[right + 1]) {
+                max_left = Math.max(max_left, height[left - 1]);
+                int min = max_left;
+                if (min > height[left]) {
+                    sum = sum + (min - height[left]);
+                }
                 left++;
-            }
-            while (left < right && arr[right] >= k) {
+                //从右到左更
+            } else {
+                max_right = Math.max(max_right, height[right + 1]);
+                int min = max_right;
+                if (min > height[right]) {
+                    sum = sum + (min - height[right]);
+                }
                 right--;
             }
-            swap(arr, left++, right--);
         }
-        // 注意：在上面运行时，left可能停在不比k小的子数组的第一个元素（最后left==right的位置符合arr[left] < k），
-        // 也可能停在第二个元素(最后left==right的位置符合arr[left] >=k)，因为交换完成后又执行了left++;right--
-
-        left = left - 1;// 保证能包括不比k小的子数组中的所有元素即可
-        right = arr.length - 1;
-        while (left < right) {
-            while (left < right && arr[left] <= k) {// 取等号防止left = left-1后包括了arr[left] == k的元素
-                left++;
-            }
-            while (left < right && arr[right] > k) {
-                right--;
-            }
-            swap(arr, left++, right--);
-        }
+        return sum;
     }
 
-    public static void swap(int[] nums, int n1, int n2) {
-        int tmp = nums[n1];
-        nums[n1] = nums[n2];
-        nums[n2] = tmp;
-    }
 }
