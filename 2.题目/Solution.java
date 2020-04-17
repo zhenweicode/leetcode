24c6548ddec4ca4c2d358e6a679151b45f01e4e7
xiezhenwei <xiezhenwei@meituan.com>
2020-04-17 12:44:14 +0800

    push

--- 2.题目/Solution.java
+++ 2.题目/Solution.java
@@ -1,37 +1,36 @@
 class Solution {
-    public int minMeetingRooms(int[][] intervals) {
-        if (intervals.length == 0) {
+    private void dfs(char[][] grid, int r, int c) {
+        int nr = grid.length;
+        int nc = grid[0].length;
+
+        if (r < 0 || c < 0 || r >= nr || c >= nc || grid[r][c] == '0') {
+            return;
+        }
+
+        grid[r][c] = '0';
+        dfs(grid, r - 1, c);
+        dfs(grid, r + 1, c);
+        dfs(grid, r, c - 1);
+        dfs(grid, r, c + 1);
+    }
+
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0) {
             return 0;
         }
 
-        // 按照会议结束时间有序，优先队列大小为分配的会议室数
-        PriorityQueue<Integer> allocator = new PriorityQueue<Integer>(intervals.length, new Comparator<Integer>() {
-            @Override
-            public int compare(Integer a, Integer b) {
-                return a - b;
+        int nr = grid.length;
+        int nc = grid[0].length;
+        int num_islands = 0;
+        for (int r = 0; r < nr; ++r) {
+            for (int c = 0; c < nc; ++c) {
+                if (grid[r][c] == '1') {
+                    ++num_islands;
+                    dfs(grid, r, c);
+                }
             }
-        });
-
-        // 按照会议开始时间排序
-        Arrays.sort(intervals, new Comparator<int[]>() {
-            @Override
-            public int compare(final int[] a, final int[] b) {
-                return a[0] - b[0];
-            }
-        });
-
-        allocator.add(intervals[0][1]);  // 添加第一场会议
-        for (int i = 1; i < intervals.length; i++) {
-
-            // 找到第一个结束的会议室分配
-            if (intervals[i][0] >= allocator.peek()) {
-                allocator.poll();
-            }
-
-            // 新会议加入
-            allocator.add(intervals[i][1]);
         }
 
-        return allocator.size();
+        return num_islands;
     }
 }
