5f80bacde07a8cfb26f95d04ae7870e6d16b40a2
xiezhenwei <xiezhenwei@meituan.com>
2020-04-17 12:09:53 +0800

    push

--- 2.题目/Solution.java
+++ 2.题目/Solution.java
@@ -1,15 +1,37 @@
 class Solution {
-    public boolean canAttendMeetings(int[][] intervals) {
-        Arrays.sort(intervals, new Comparator<int[]>() {
-            public int compare(int[] i1, int[] i2) {
-                return i1[0] - i2[0];
+    public int minMeetingRooms(int[][] intervals) {
+        if (intervals.length == 0) {
+            return 0;
+        }
+
+        // 按照会议结束时间有序，优先队列大小为分配的会议室数
+        PriorityQueue<Integer> allocator = new PriorityQueue<Integer>(intervals.length, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer a, Integer b) {
+                return a - b;
             }
         });
 
-        for (int i = 0; i < intervals.length - 1; i++) {
-            if (intervals[i][1] > intervals[i + 1][0])
-                return false;
+        // 按照会议开始时间排序
+        Arrays.sort(intervals, new Comparator<int[]>() {
+            @Override
+            public int compare(final int[] a, final int[] b) {
+                return a[0] - b[0];
+            }
+        });
+
+        allocator.add(intervals[0][1]);  // 添加第一场会议
+        for (int i = 1; i < intervals.length; i++) {
+
+            // 找到第一个结束的会议室分配
+            if (intervals[i][0] >= allocator.peek()) {
+                allocator.poll();
+            }
+
+            // 新会议加入
+            allocator.add(intervals[i][1]);
         }
-        return true;
+
+        return allocator.size();
     }
 }
