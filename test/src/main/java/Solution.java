d93b08dd71ad592cff848bda9531b9cac84f61f2
xiezhenwei <xiezhenwei@meituan.com>
2020-04-18 23:52:02 +0800

    push

--- test/src/main/java/Solution.java
+++ test/src/main/java/Solution.java
@@ -1,17 +1,48 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 class Solution {
-    public List<Integer> grayCode(int n) {
-        List<Integer> res = new ArrayList<>();
-        res.add(0);
-        int head = 1;
-        for (int i = 0; i < n; i++) {
-            for (int j = res.size() - 1; j >= 0; j--)
-                res.add(head + res.get(j));
-            head <<= 1;
+    public String minWindow(String s, String t) {
+        if (s.length() == 0 || t.length() == 0) {
+            return "";
         }
-        return res;
+
+        Map<Character, Integer> dictT = new HashMap<>();
+        Map<Character, Integer> windowCounts = new HashMap<>();
+        for (int i = 0; i < t.length(); i++) {
+            dictT.put(t.charAt(i), dictT.getOrDefault(t.charAt(i), 0) + 1);
+        }
+
+        int left = 0, right = 0;
+        int required = dictT.size();   // 需要的不重复字符数
+        int formed = 0;  // 实际的不重复字符数，比如ABB，则formed=2
+
+        int[] ans = {-1, 0, 0};  // ans list of the form (window length, left, right)
+        while (right < s.length()) {
+            char c = s.charAt(right);
+            windowCounts.put(c, windowCounts.getOrDefault(c, 0) + 1);
+            if (dictT.containsKey(c) && windowCounts.get(c).intValue() == dictT.get(c).intValue()) {  // 去掉intValue会报错，使用new关键字创建的包装类型对象，不可以直接使用“==”来做大小比较，但可以使用“<”、“>”、“<=”、“>=”来做大小比较。
+                formed++;
+            }
+
+            while (left <= right && formed == required) {
+                c = s.charAt(left);
+                if (ans[0] == -1 || right - left + 1 < ans[0]) {
+                    ans[0] = right - left + 1;
+                    ans[1] = left;
+                    ans[2] = right;
+                }
+
+                windowCounts.put(c, windowCounts.get(c) - 1);
+                if (dictT.containsKey(c) && windowCounts.get(c) < dictT.get(c)) {
+                    formed--;
+                }
+
+                left++;
+            }
+
+            right++;
+        }
+
+        return ans[0] == -1 ? "" : s.substring(ans[1], ans[2] + 1);
     }
 }
