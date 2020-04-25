c20b5feb54fb85b0538676218fa145eb451c0b80
xiezhenwei <xiezhenwei@meituan.com>
2020-04-25 12:15:17 +0800

    init

--- test/src/main/java/Solution.java
+++ test/src/main/java/Solution.java
@@ -1,55 +1,79 @@
 import java.util.*;
 
 class Solution {
-    public int calculate(String s) {
-        Stack<Integer> stack = new Stack<>();
-        int operand = 0;   // 当前数字，如果遇到连续的数字，比如123，需要123 >> 120 + 3 >> 100 + 20 + 3
-        int result = 0; // 最终结果
-        int sign = 1;  // 1正0负
-        int[] dp = new int[12];
-        int length = dp.length;
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
+    public static int calculate(String s) {
+        /*
+            将 减法、乘法、除法 转换为 加法
+            某个数 num, 如果前面的对应的运算符是 -，那么 将 -num 压入栈中
+            这样，我们只需在最后将栈的元素全部弹出，完成加法操作，即可得到最终结果
+
+            对于括号，它存在递归性质
+            即
+            3 * (2 + 4 * 3) + 2
+          = 3 * calculate(2 + 4 * 3) + 2
+          = 3 * 24 + 2
+          即我们可以将括号内的字符串当作一个运算式，再递归调用本函数，最终返回一个数值
+        */
+        int[] i = new int[1];
+        return dfs(s, i);
+    }
+
+    private static int dfs(String s, int[] i) {
+        Deque<Integer> stack = new LinkedList<>();
+
+        //记录某个连续的数，比如 "42"，那么我们首先 num = 4，然后遇到 2 ,num = num * 10 + 2 = 42
+        int num = 0;
+        char op = '+';
+        for (; i[0] < s.length(); i[0]++) {
+            char ch = s.charAt(i[0]);
+
+            //遇到左括号，递归运算内部子式
+            if (ch == '(') {
+                ++i[0];
+                num = dfs(s, i);
+            }
+
             if (Character.isDigit(ch)) {
-                operand = 10 * operand + (int) (ch - '0');
-            } else if (ch == '+') {
-                result += sign * operand;
-                sign = 1;
-                operand = 0;
-            } else if (ch == '-') {
-                result += sign * operand;
-                sign = -1;
-                operand = 0;
-            } else if (ch == '(') {
-                // Push the result and sign on to the stack, for later
-                // We push the result first, then sign
-                stack.push(result);
-                stack.push(sign);
-
-                // Reset operand and result, as if new evaluation begins for the new sub-expression
-                sign = 1;
-                result = 0;
-
-            } else if (ch == ')') {
-
-                // Evaluate the expression to the left
-                // with result, sign and operand
-                result += sign * operand;
-
-                // ')' marks end of expression within a set of parenthesis
-                // Its result is multiplied with sign on top of stack
-                // as stack.pop() is the sign before the parenthesis
-                result *= stack.pop();
-
-                // Then add to the next operand on the top.
-                // as stack.pop() is the result calculated before this parenthesis
-                // (operand on stack) + (sign on stack * (result from parenthesis))
-                result += stack.pop();
-
-                // Reset the operand
-                operand = 0;
+                num = num * 10 + (ch - '0');
+            }
+            //不是数字，不是空格（运算符 或 '(' 或 ')' ） 或者 到了最后一个字符，那么根据前面记录的 op 操作符 将数字压栈，然后将新的运算符 ch 赋值给 op
+            if (!Character.isDigit(ch) && ch != ' ' || i[0] == s.length() - 1) {
+                switch (op) {
+                    case '+':
+                        stack.push(num);
+                        break;
+                    case '-':
+                        stack.push(-num);
+                        break;
+                    case '*':
+                        int pre = stack.pop();
+                        stack.push(pre * num);
+                        break;
+                    case '/':
+                        pre = stack.pop();
+                        stack.push(pre / num);
+                        break;
+                }
+                num = 0;
+                op = ch;
+            }
+            /*
+            遇到右括号，退出循环，然后计算结果， 返回上一层 dfs
+            这一步写在最后是因为，当 ch 为 右括号 时，那么我们需要先将前面已经得到的 num 压入栈中，再退出循环
+            */
+            if (ch == ')') {
+                break;
             }
         }
-        return result + (sign * operand);
+        int res = 0;
+        while (!stack.isEmpty()) {
+            res += stack.pop();
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(calculate("3+2*2"));
+        System.out.println(calculate("3+2*2 + 5*(3+2)"));
     }
 }
