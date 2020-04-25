752f6f6e4e9f0dd7b6e8a66f2fe023d99bd3a150
xiezhenwei <xiezhenwei@meituan.com>
2020-04-25 11:42:41 +0800

    init

--- test/src/main/java/Solution.java
+++ test/src/main/java/Solution.java
@@ -2,103 +2,54 @@ import java.util.*;
 
 class Solution {
     public int calculate(String s) {
-        String[] polish = getPolish(s); //转后缀表达式
-        return evalRPN(polish);
-    }
+        Stack<Integer> stack = new Stack<>();
+        int operand = 0;   // 当前数字，如果遇到连续的数字，比如123，需要123 >> 120 + 3 >> 100 + 20 + 3
+        int result = 0; // 最终结果
+        int sign = 1;  // 1正0负
+        int[] dp = new int[12];
+        int length = dp.length;
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (Character.isDigit(ch)) {
+                operand = 10 * operand + (int) (ch - '0');
+            } else if (ch == '+') {
+                result += sign * operand;
+                sign = 1;
+                operand = 0;
+            } else if (ch == '-') {
+                result += sign * operand;
+                sign = -1;
+                operand = 0;
+            } else if (ch == '(') {
+                // Push the result and sign on to the stack, for later
+                // We push the result first, then sign
+                stack.push(result);
+                stack.push(sign);
 
-    //中缀表达式转后缀表达式
-    private String[] getPolish(String s) {
-        List<String> res = new ArrayList<>();
-        Stack<String> stack = new Stack<>();
-        char[] array = s.toCharArray();
-        int n = array.length;
-        int temp = -1; //累加数字，-1 表示当前没有数字
-        for (int i = 0; i < n; i++) {
-            if (array[i] == ' ') {
-                continue;
-            }
-            //遇到数字
-            if (isNumber(array[i])) {
-                //进行数字的累加
-                if (temp == -1) {
-                    temp = array[i] - '0';
-                } else {
-                    temp = temp * 10 + array[i] - '0';
-                }
-            } else {
-                //遇到其它操作符，将数字加入到结果中
-                if (temp != -1) {
-                    res.add(temp + "");
-                    temp = -1;
-                }
-                if (isOperation(array[i] + "")) {
-                    //遇到操作符将栈中的操作符加入到结果中
-                    while (!stack.isEmpty()) {
-                        //遇到左括号结束
-                        if (stack.peek().equals("(")) {
-                            break;
-                        }
-                        res.add(stack.pop());
-                    }
-                    //当前操作符入栈
-                    stack.push(array[i] + "");
-                } else {
-                    //遇到左括号，直接入栈
-                    if (array[i] == '(') {
-                        stack.push(array[i] + "");
-                    }
-                    //遇到右括号，将出栈元素加入到结果中，直到遇到左括号
-                    if (array[i] == ')') {
-                        while (!stack.peek().equals("(")) {
-                            res.add(stack.pop());
-                        }
-                        //左括号出栈
-                        stack.pop();
-                    }
+                // Reset operand and result, as if new evaluation begins for the new sub-expression
+                sign = 1;
+                result = 0;
 
-                }
+            } else if (ch == ')') {
+
+                // Evaluate the expression to the left
+                // with result, sign and operand
+                result += sign * operand;
+
+                // ')' marks end of expression within a set of parenthesis
+                // Its result is multiplied with sign on top of stack
+                // as stack.pop() is the sign before the parenthesis
+                result *= stack.pop();
+
+                // Then add to the next operand on the top.
+                // as stack.pop() is the result calculated before this parenthesis
+                // (operand on stack) + (sign on stack * (result from parenthesis))
+                result += stack.pop();
+
+                // Reset the operand
+                operand = 0;
             }
         }
-        //如果有数字，将数字加入到结果
-        if (temp != -1) {
-            res.add(temp + "");
-        }
-        //栈中的其他元素加入到结果
-        while (!stack.isEmpty()) {
-            res.add(stack.pop());
-        }
-        String[] sArray = new String[res.size()];
-        //List 转为 数组
-        for (int i = 0; i < res.size(); i++) {
-            sArray[i] = res.get(i);
-        }
-        return sArray;
-    }
-
-    // 下边是 150 题的代码，求后缀表达式的值
-    public static int evalRPN(String[] tokens) {
-        int[] numStack = new int[tokens.length / 2 + 1];
-        int index = 0;
-        for (String s : tokens) {
-            switch (s) {
-                case "+":
-                    numStack[index - 2] += numStack[--index];
-                    break;
-                case "-":
-                    numStack[index - 2] -= numStack[--index];
-                    break;
-                case "*":
-                    numStack[index - 2] *= numStack[--index];
-                    break;
-                case "/":
-                    numStack[index - 2] /= numStack[--index];
-                    break;
-                default:
-                    // numStack[index++] = Integer.valueOf(s); valueOf改为parseInt，减少自动拆箱装箱操作
-                    numStack[index++] = Integer.parseInt(s);
-                    break;
-            }
-        }
-        return numStack[0];
+        return result + (sign * operand);
     }
 }
