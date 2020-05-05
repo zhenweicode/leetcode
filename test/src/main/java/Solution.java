8a454199ac3c6f79b8092833bb6fceb156d1cf4f
xiezhenwei <xiezhenwei@meituan.com>
2020-05-05 10:41:04 +0800

    init

--- test/src/main/java/Solution.java
+++ test/src/main/java/Solution.java
@@ -1,15 +1,17 @@
 class Solution {
-    public int reverse(int x) {
+    public static int reverse(int x) {
         int ans = 0;
         while (x != 0) {
             int pop = x % 10;
-            if (ans > Integer.MAX_VALUE / 10 || (ans == Integer.MAX_VALUE / 10 && pop > 7))
-                return 0;
-            if (ans < Integer.MIN_VALUE / 10 || (ans == Integer.MIN_VALUE / 10 && pop < -8))
+            if (ans > Integer.MAX_VALUE / 10 || ans < Integer.MIN_VALUE / 10)
                 return 0;
             ans = ans * 10 + pop;
             x /= 10;
         }
         return ans;
+    }
+
+    public static void main(String[] args) {
+        reverse(Integer.MIN_VALUE);
     }
 }
