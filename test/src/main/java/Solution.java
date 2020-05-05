033420d8c86efc9eca45dfdce0a46deb09b2afe0
xiezhenwei <xiezhenwei@meituan.com>
2020-05-05 19:49:39 +0800

    init

--- test/src/main/java/Solution.java
+++ test/src/main/java/Solution.java
@@ -1,33 +1,33 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 
-public class Solution {
-    public void nextPermutation(int[] nums) {
-        int i = nums.length - 2;
-        while (i >= 0 && nums[i + 1] <= nums[i]) {
-            i--;
-        }
-        if (i >= 0) {
-            int j = nums.length - 1;
-            while (j >= 0 && nums[j] <= nums[i]) {
-                j--;
+class Solution {
+    public int[][] merge(int[][] arr) {
+        if (arr == null || arr.length <= 1)
+            return arr;
+        List<int[]> list = new ArrayList<>();
+        //Arrays.sort(arr,(a,b)->a[0]-b[0]);
+        Arrays.sort(arr, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] a, int[] b) {
+                return a[0] - b[0];
             }
-            swap(nums, i, j);
-        }
-        reverse(nums, i + 1);
-    }
+        });
 
-    private void reverse(int[] nums, int start) {
-        int i = start, j = nums.length - 1;
-        while (i < j) {
-            swap(nums, i, j);
+        int i = 0;
+        int n = arr.length;
+        while (i < n) {
+            int left = arr[i][0];
+            int right = arr[i][1];
+            while (i < n - 1 && right >= arr[i + 1][0]) {
+                right = Math.max(right, arr[i + 1][1]);
+                i++;
+            }
+            list.add(new int[]{left, right});
             i++;
-            j--;
         }
-    }
-
-    private void swap(int[] nums, int i, int j) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
+        return list.toArray(new int[list.size()][2]);
     }
 }
