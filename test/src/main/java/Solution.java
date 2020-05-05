c5987e0cb6d6dbc7b6f27e213bf9820165e8b8d6
xiezhenwei <xiezhenwei@meituan.com>
2020-05-05 15:54:15 +0800

    init

--- test/src/main/java/Solution.java
+++ test/src/main/java/Solution.java
@@ -1,39 +1,18 @@
 import java.util.*;
 
-public class Trie {
-    private boolean is_string = false;
-    private Trie[] next = new Trie[26];
-
-    public Trie() {
-    }
-
-    public void insert(String word) {//插入单词
-        Trie root = this;
-        char[] w = word.toCharArray();
-        for (int i = 0; i < w.length; ++i) {
-            if (root.next[w[i] - 'a'] == null) root.next[w[i] - 'a'] = new Trie();
-            root = root.next[w[i] - 'a'];
+class Solution {
+    public String convert(String s, int numRows) {
+        if (numRows < 2) return s;
+        List<StringBuilder> rows = new ArrayList<>();
+        for (int i = 0; i < numRows; i++) rows.add(new StringBuilder());
+        int i = 0, flag = -1;
+        for (char c : s.toCharArray()) {
+            rows.get(i).append(c);
+            if (i == 0 || i == numRows - 1) flag = -flag;
+            i += flag;
         }
-        root.is_string = true;
-    }
-
-    public boolean search(String word) {//查找单词
-        Trie root = this;
-        char w[] = word.toCharArray();
-        for (int i = 0; i < w.length; ++i) {
-            if (root.next[w[i] - 'a'] == null) return false;
-            root = root.next[w[i] - 'a'];
-        }
-        return root.is_string;
-    }
-
-    public boolean startsWith(String prefix) {//查找前缀
-        Trie root = this;
-        char p[] = prefix.toCharArray();
-        for (int i = 0; i < p.length; ++i) {
-            if (root.next[p[i] - 'a'] == null) return false;
-            root = root.next[p[i] - 'a'];
-        }
-        return true;
+        StringBuilder res = new StringBuilder();
+        for (StringBuilder row : rows) res.append(row);
+        return res.toString();
     }
 }
