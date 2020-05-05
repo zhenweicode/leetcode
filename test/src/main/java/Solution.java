cf3a495ffcb351cd3f07a02e9bea0382cd927e5b
xiezhenwei <xiezhenwei@meituan.com>
2020-05-05 10:26:55 +0800

    init

--- test/src/main/java/Solution.java
+++ test/src/main/java/Solution.java
@@ -1,52 +1,15 @@
 class Solution {
-    int left_bound(int[] nums, int target) {
-        int left = 0, right = nums.length - 1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] < target) {
-                left = mid + 1;
-            } else if (nums[mid] > target) {
-                right = mid - 1;
-            } else if (nums[mid] == target) {
-                // 别返回，锁定左侧边界
-                right = mid - 1;
-            }
+    public int reverse(int x) {
+        int ans = 0;
+        while (x != 0) {
+            int pop = x % 10;
+            if (ans > Integer.MAX_VALUE / 10 || (ans == Integer.MAX_VALUE / 10 && pop > 7))
+                return 0;
+            if (ans < Integer.MIN_VALUE / 10 || (ans == Integer.MIN_VALUE / 10 && pop < -8))
+                return 0;
+            ans = ans * 10 + pop;
+            x /= 10;
         }
-        // 最后要检查 left 越界的情况
-        if (left >= nums.length || nums[left] != target)
-            return -1;
-        return left;
-    }
-
-
-    int right_bound(int[] nums, int target) {
-        int left = 0, right = nums.length - 1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] < target) {
-                left = mid + 1;
-            } else if (nums[mid] > target) {
-                right = mid - 1;
-            } else if (nums[mid] == target) {
-                // 别返回，锁定右侧边界
-                left = mid + 1;
-            }
-        }
-        // 最后要检查 right 越界的情况
-        if (right < 0 || nums[right] != target)
-            return -1;
-        return right;
-    }
-
-
-    public int[] searchRange(int[] nums, int target) {
-        int[] targetRange = {-1, -1};
-        targetRange[0] = left_bound(nums, target);
-        targetRange[1] = right_bound(nums, target);
-        return targetRange;
-    }
-
-    public static void main(String[] args) {
-        new Solution().searchRange(new int[]{5, 7, 7, 8, 8, 10}, 8);
+        return ans;
     }
 }
