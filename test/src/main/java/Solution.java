40e935b639f234e46d793c795762cdc50b287a86
xiezhenwei <xiezhenwei@meituan.com>
2020-05-17 20:55:03 +0800

    init

--- test/src/main/java/Solution.java
+++ test/src/main/java/Solution.java
@@ -3,31 +3,29 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 
-class Solution {
-    public int[][] merge(int[][] arr) {
-        if (arr == null || arr.length <= 1)
-            return arr;
-        List<int[]> list = new ArrayList<>();
-        //Arrays.sort(arr,(a,b)->a[0]-b[0]);
-        Arrays.sort(arr, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] a, int[] b) {
-                return a[0] - b[0];
-            }
-        });
-
-        int i = 0;
-        int n = arr.length;
-        while (i < n) {
-            int left = arr[i][0];
-            int right = arr[i][1];
-            while (i < n - 1 && right >= arr[i + 1][0]) {
-                right = Math.max(right, arr[i + 1][1]);
-                i++;
-            }
-            list.add(new int[]{left, right});
-            i++;
+class Solution{
+    public static int biSearch(int[] nums, int target){
+        if(nums == null || nums.length == 0){
+            return -1;
         }
-        return list.toArray(new int[list.size()][2]);
+
+        if(target < nums[0] || target > nums[nums.length - 1]){
+            return -1;
+        }
+
+        int low = 0;
+        int high = nums.length - 1;
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+            if(target == nums[mid]){
+                return mid;
+            } else if(target > nums[mid]){
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return -1;
     }
 }
