919eb2e3c44876fc8f4a32582ccb4531f1fb9201
xiezhenwei <xiezhenwei@meituan.com>
2020-05-30 00:14:17 +0800

    init

--- test/src/main/java/Solution.java
+++ test/src/main/java/Solution.java
@@ -1,31 +1,24 @@
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-class Solution{
-    public static int biSearch(int[] nums, int target){
-        if(nums == null || nums.length == 0){
-            return -1;
-        }
-
-        if(target < nums[0] || target > nums[nums.length - 1]){
-            return -1;
-        }
-
-        int low = 0;
-        int high = nums.length - 1;
-        while(low <= high){
-            int mid = low + (high - low) / 2;
-            if(target == nums[mid]){
-                return mid;
-            } else if(target > nums[mid]){
-                low = mid + 1;
-            } else {
-                high = mid - 1;
+public class Solution {
+    public static int lengthOfLongestSubstring(String s) {
+        int n = s.length(), ans = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        for (int j = 0, i = 0; j < n; j++) {
+            if (map.containsKey(s.charAt(j))) {
+                i = map.get(s.charAt(j)) + 1; // 由于map没有remove操作，防止i向左滑动
             }
-        }
 
-        return -1;
+            ans = Math.max(ans, j - i + 1);  // j - i + 1代表长度，上面解法先执行了j++
+            map.put(s.charAt(j), j);
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(lengthOfLongestSubstring("abba"));
     }
 }
