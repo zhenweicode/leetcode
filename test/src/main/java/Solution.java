be5af4fd717e67a85fadf590cddb0acc9d815d73
xiezhenwei <xiezhenwei@meituan.com>
2020-05-30 13:44:47 +0800

    init

--- test/src/main/java/Solution.java
+++ test/src/main/java/Solution.java
@@ -1,20 +1,29 @@
 import java.util.*;
 
 class Solution {
-    public ListNode swapPairs(ListNode head) {
-        ListNode dummy = new ListNode(0);
-        dummy.next = head;
-        ListNode temp = dummy;
-        while (temp.next != null && temp.next.next != null) {
-            ListNode first = temp.next;
-            ListNode second = first.next;
-            ListNode third = second.next;
-            second.next = first;
-            first.next = third;
-            temp.next = second;
-            temp = first; // 翻转之后，first后面是third
-        }
+    public int divide(int dividend, int divisor) { // 被除数 除数
+        if (divisor == -1 && dividend == Integer.MIN_VALUE) return Integer.MAX_VALUE; // 溢出
+        int sign = 1;
+        if ((dividend > 0 && divisor < 0) || (dividend < 0 && divisor > 0))
+            sign = -1;
+        if (divisor == 1) return dividend;
+        if (divisor == -1) return -dividend;
+        int a = dividend > 0 ? -dividend : dividend;
+        int b = divisor > 0 ? -divisor : divisor;
+        // 都改为负号是因为int 的范围是[2^32, 2^32-1]，如果a是-2^32，转为正数时将会溢出
+        if (a > b) return 0;
+        int ans = div(a, b);
+        return sign == -1 ? -ans : ans;
+    }
 
-        return dummy.next;
+    private int div(int a, int b) {
+        if (a > b) return 0;
+        int count = 1;
+        int tb = b;
+        while (tb + tb >= a && tb + tb < 0) { // 溢出之后不再小于0
+            tb += tb;
+            count += count;
+        }
+        return count + div(a - tb, b);
     }
 }
