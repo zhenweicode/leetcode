d782a28a46c97f3ee2648b93f83737a565ffa3d0
xiezhenwei <xiezhenwei@meituan.com>
2020-05-30 16:24:34 +0800

    init

--- test/src/main/java/Solution.java
+++ test/src/main/java/Solution.java
@@ -1,27 +1,28 @@
 import java.util.*;
 
-class Solution {
-    public boolean isValidSudoku(char[][] board) {
-        int[][] rows = new int[9][9];
-        int[][] col = new int[9][9];
-        int[][] sbox = new int[9][9];
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[0].length; j++) {
-                if (board[i][j] != '.') {
-                    int num = board[i][j] - '1';
-                    int index_box = (i / 3) * 3 + j / 3;
-                    if (rows[i][num] == 1) return false;
-                    else rows[i][num] = 1;
-
-                    if (col[j][num] == 1) return false;
-                    else col[j][num] = 1;
-
-                    if (sbox[index_box][num] == 1) return false;
-                    else sbox[index_box][num] = 1;
-
-                }
+public class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int len = nums.length;
+        for (int i = 0; i < len; i++) {
+            while (nums[i] > 0 && nums[i] <= len && nums[nums[i] - 1] != nums[i]) {
+                // 满足在指定范围内、并且没有放在正确的位置上，才交换
+                // 例如：数值 3 应该放在索引 2 的位置上
+                swap(nums, nums[i] - 1, i);
             }
         }
-        return true;
+
+        for (int i = 0; i < len; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+        // 都正确则返回数组长度 + 1
+        return len + 1;
+    }
+
+    private void swap(int[] nums, int index1, int index2) {
+        int temp = nums[index1];
+        nums[index1] = nums[index2];
+        nums[index2] = temp;
     }
 }
