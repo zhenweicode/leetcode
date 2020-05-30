ef2cec6b00fa79eead96f51008f37f8f24d2b632
xiezhenwei <xiezhenwei@meituan.com>
2020-05-30 10:08:28 +0800

    init

--- test/src/main/java/Solution.java
+++ test/src/main/java/Solution.java
@@ -3,14 +3,25 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-class Solution {
-    public int maxArea(int[] height) {
-        int i = 0, j = height.length - 1, res = 0;
-        while (i < j) {
-            res = height[i] < height[j] ?
-                    Math.max(res, (j - i) * height[i++]) :
-                    Math.max(res, (j - i) * height[j--]);
+public class Solution {
+
+    public String intToRoman(int num) {
+        // 把阿拉伯数字与罗马数字可能出现的所有情况和对应关系，放在两个数组中
+        // 并且按照阿拉伯数字的大小降序排列，这是贪心选择思想
+        int[] nums = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] romans = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+        StringBuilder stringBuilder = new StringBuilder();
+        int index = 0;
+        while (index < 13) {
+            // 特别注意：这里是等号
+            while (num >= nums[index]) {
+                // 注意：这里是等于号，表示尽量使用大的"面值"
+                stringBuilder.append(romans[index]);
+                num -= nums[index];
+            }
+            index++;
         }
-        return res;
+        return stringBuilder.toString();
     }
 }
