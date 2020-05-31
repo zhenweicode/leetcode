d68bde5fbb79a070a92ab24b85e9f4174c9f4ea0
xiezhenwei <xiezhenwei@meituan.com>
2020-05-31 18:33:33 +0800

    init

--- test/src/main/java/Solution.java
+++ test/src/main/java/Solution.java
@@ -2,25 +2,44 @@ import java.util.*;
 
 
 class Solution {
-    public void flatten(TreeNode root) {
-        while (root != null) {
-            //左子树为 null，直接考虑下一个节点
-            if (root.left == null) {
-                root = root.right;
-            } else {
-                // 找左子树最右边的节点
-                TreeNode pre = root.left;
-                while (pre.right != null) {
-                    pre = pre.right;
+    public int maxProfit(int[] prices) {
+        return maxProfit(2, prices);
+    }
+
+    public int maxProfit(int k, int[] prices) {
+        int n = prices.length;
+        if (k > n / 2)
+            return greedy(prices);
+
+        int[][][] dp = new int[n][k + 1][2];
+
+        for (int i = 0; i < n; i++) {
+            // base case
+            dp[i][0][0] = 0;//至今为止没有交易，收益为0
+            dp[i][0][1] = Integer.MIN_VALUE;//交易了0次，但持有股票，不符合规则
+
+            for (int j = k; j >= 1; j--) {
+                if (i == 0) {
+                    dp[i][j][0] = 0;//第一天买入t次，当天卖出t次,收入为0
+                    dp[i][j][1] = -prices[i];// 甭管第一天买多少次，反正最后少卖一次，持有了股票
+                    continue;
                 }
-                //将原来的右子树接到左子树的最右边节点
-                pre.right = root.right;
-                // 将左子树插入到右子树的地方
-                root.right = root.left;
-                root.left = null;
-                // 考虑下一个节点
-                root = root.right;
+
+                dp[i][j][0] = Math.max(dp[i - 1][j][0], dp[i - 1][j][1] + prices[i]);
+                dp[i][j][1] = Math.max(dp[i - 1][j][1], dp[i - 1][j - 1][0] - prices[i]);
             }
         }
+
+
+        return dp[n - 1][k][0];
+    }
+
+    private int greedy(int[] prices) {
+        int max = 0;
+        for (int i = 1; i < prices.length; ++i) {
+            if (prices[i] > prices[i - 1])
+                max += prices[i] - prices[i - 1];
+        }
+        return max;
     }
 }
