84ae35f80bdbd063c70b3ddd1fbf5a8c8aca7237
xiezhenwei <xiezhenwei@meituan.com>
2020-05-31 14:16:43 +0800

    init

--- test/src/main/java/Solution.java
+++ test/src/main/java/Solution.java
@@ -1,6 +1,27 @@
 import java.util.*;
 
 class Solution {
+    public int maximalRectangle(char[][] matrix) {
+        if (matrix.length == 0) {
+            return 0;
+        }
+        int[] heights = new int[matrix[0].length];
+        int maxArea = 0;
+        for (int row = 0; row < matrix.length; row++) {
+            //遍历每一列，更新高度
+            for (int col = 0; col < matrix[0].length; col++) {
+                if (matrix[row][col] == '1') {
+                    heights[col] += 1;
+                } else {
+                    heights[col] = 0;
+                }
+            }
+            //调用上一题的解法，更新函数
+            maxArea = Math.max(maxArea, largestRectangleArea(heights));
+        }
+        return maxArea;
+    }
+
     public int largestRectangleArea(int[] heights) {
         int n = heights.length;
         int[] left = new int[n];  // 第i个元素表示i左侧第一个比他矮的下标
