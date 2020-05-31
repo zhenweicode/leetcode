1e69440f61c2490735fa4f5e4e6b269870f59011
xiezhenwei <xiezhenwei@meituan.com>
2020-05-31 10:29:24 +0800

    init

--- test/src/main/java/Solution.java
+++ test/src/main/java/Solution.java
@@ -1,38 +1,23 @@
 import java.util.*;
 
 class Solution {
-    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        int row = obstacleGrid.length;
-        int column = obstacleGrid[0].length;
+    public String simplifyPath(String path) {
+        String[] s = path.split("/");
+        Stack<String> stack = new Stack<>();
 
-        // 开始路径有障碍物
-        if (obstacleGrid[0][0] == 1) {
-            return 0;
+        for (int i = 0; i < s.length; i++) {
+            if (!stack.isEmpty() && s[i].equals(".."))
+                stack.pop();
+            else if (!s[i].equals("") && !s[i].equals(".") && !s[i].equals(".."))
+                stack.push(s[i]);
         }
+        if (stack.isEmpty())
+            return "/";
 
-        // 起始位置
-        obstacleGrid[0][0] = 1;
-
-        // 填充第一列
-        for (int i = 1; i < row; i++) {
-            obstacleGrid[i][0] = (obstacleGrid[i][0] == 0 && obstacleGrid[i - 1][0] == 1) ? 1 : 0;
+        StringBuilder res = new StringBuilder();
+        for (int i = 0; i < stack.size(); i++) {
+            res.append("/").append(stack.get(i));
         }
-
-        // 填充第一行
-        for (int i = 1; i < column; i++) {
-            obstacleGrid[0][i] = (obstacleGrid[0][i] == 0 && obstacleGrid[0][i - 1] == 1) ? 1 : 0;
-        }
-
-        for (int i = 1; i < row; i++) {
-            for (int j = 1; j < column; j++) {
-                if (obstacleGrid[i][j] == 0) {
-                    obstacleGrid[i][j] = obstacleGrid[i - 1][j] + obstacleGrid[i][j - 1];
-                } else {
-                    obstacleGrid[i][j] = 0;
-                }
-            }
-        }
-
-        return obstacleGrid[row - 1][column - 1];
+        return res.toString();
     }
 }
