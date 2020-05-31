8dd93fe5f808f8f3eb39e32c9edee9a5cc40f6b6
xiezhenwei <xiezhenwei@meituan.com>
2020-05-31 17:06:16 +0800

    init

--- test/src/main/java/Solution.java
+++ test/src/main/java/Solution.java
@@ -1,18 +1,26 @@
 import java.util.*;
 
-class Solution {
-    public int minDepth(TreeNode root) {
-        if (root == null) return 0;
-        //这道题递归条件里分为三种情况
-        //1.左孩子和右孩子都为空的情况，说明到达了叶子节点，直接返回1即可
-        if (root.left == null && root.right == null) return 1;
-        //2.如果左孩子和右孩子其中一个为空，那么需要返回比较大的那个孩子的深度
-        int m1 = minDepth(root.left);
-        int m2 = minDepth(root.right);
-        //这里其中一个节点为空，说明m1和m2有一个必然为0，所以可以返回m1 + m2 + 1;
-        if (root.left == null || root.right == null) return m1 + m2 + 1;
 
-        //3.最后一种情况，也就是左右孩子都不为空，返回最小深度+1即可
-        return Math.min(m1, m2) + 1;
+class Solution {
+    public void flatten(TreeNode root) {
+        while (root != null) {
+            //左子树为 null，直接考虑下一个节点
+            if (root.left == null) {
+                root = root.right;
+            } else {
+                // 找左子树最右边的节点
+                TreeNode pre = root.left;
+                while (pre.right != null) {
+                    pre = pre.right;
+                }
+                //将原来的右子树接到左子树的最右边节点
+                pre.right = root.right;
+                // 将左子树插入到右子树的地方
+                root.right = root.left;
+                root.left = null;
+                // 考虑下一个节点
+                root = root.right;
+            }
+        }
     }
 }
