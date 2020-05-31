7c7a84e850e1ad546a504d339884bd838aa5dff2
xiezhenwei <xiezhenwei@meituan.com>
2020-05-31 10:56:30 +0800

    init

--- test/src/main/java/Solution.java
+++ test/src/main/java/Solution.java
@@ -1,23 +1,45 @@
 import java.util.*;
 
-class Solution {
-    public String simplifyPath(String path) {
-        String[] s = path.split("/");
-        Stack<String> stack = new Stack<>();
+public class Solution {
 
-        for (int i = 0; i < s.length; i++) {
-            if (!stack.isEmpty() && s[i].equals(".."))
-                stack.pop();
-            else if (!s[i].equals("") && !s[i].equals(".") && !s[i].equals(".."))
-                stack.push(s[i]);
+    public void sortColors(int[] nums) {
+        int len = nums.length;
+        if (len < 2) {
+            return;
         }
-        if (stack.isEmpty())
-            return "/";
 
-        StringBuilder res = new StringBuilder();
-        for (int i = 0; i < stack.size(); i++) {
-            res.append("/").append(stack.get(i));
+        // all in [0, zero) = 0
+        // all in [zero, i) = 1
+        // all in [two, len - 1] = 2
+
+        // 循环终止条件是 i == two，那么循环可以继续的条件是 i < two
+        // 为了保证初始化的时候 [0, zero) 为空，设置 zero = 0，
+        // 所以下面遍历到 0 的时候，先交换，再加
+        int zero = 0;
+
+        // 为了保证初始化的时候 [two, len - 1] 为空，设置 two = len
+        // 所以下面遍历到 2 的时候，先减，再交换
+        int two = len;
+        int i = 0;
+        // 当 i == two 上面的三个子区间正好覆盖了全部数组
+        // 因此，循环可以继续的条件是 i < two
+        while (i < two) {
+            if (nums[i] == 0) {
+                swap(nums, i, zero);
+                zero++;
+                i++;
+            } else if (nums[i] == 1) {
+                i++;
+            } else {
+                two--;
+                swap(nums, i, two);
+            }
         }
-        return res.toString();
+    }
+
+    private void swap(int[] nums, int index1, int index2) {
+        int temp = nums[index1];
+        nums[index1] = nums[index2];
+        nums[index2] = temp;
     }
 }
