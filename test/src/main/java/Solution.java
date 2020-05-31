35131141d835d5de2cf47fefdace7d038a7ed121
xiezhenwei <xiezhenwei@meituan.com>
2020-05-31 16:49:56 +0800

    init

--- test/src/main/java/Solution.java
+++ test/src/main/java/Solution.java
@@ -1,24 +1,18 @@
 import java.util.*;
 
 class Solution {
-    public TreeNode sortedListToBST(ListNode head) {
-        if (head == null) return null;
-        if (head.next == null) return new TreeNode(head.val);
-        ListNode pre = head;
-        ListNode p = pre.next;
-        ListNode q = p.next;
-        //找到链表的中点p
-        while (q != null && q.next != null) {
-            pre = pre.next;
-            p = pre.next;
-            q = q.next.next;
-        }
+    public int minDepth(TreeNode root) {
+        if (root == null) return 0;
+        //这道题递归条件里分为三种情况
+        //1.左孩子和右孩子都为空的情况，说明到达了叶子节点，直接返回1即可
+        if (root.left == null && root.right == null) return 1;
+        //2.如果左孩子和右孩子其中一个为空，那么需要返回比较大的那个孩子的深度
+        int m1 = minDepth(root.left);
+        int m2 = minDepth(root.right);
+        //这里其中一个节点为空，说明m1和m2有一个必然为0，所以可以返回m1 + m2 + 1;
+        if (root.left == null || root.right == null) return m1 + m2 + 1;
 
-        // 将中点左边的链表分开
-        pre.next = null;
-        TreeNode root = new TreeNode(p.val);
-        root.left = sortedListToBST(head);
-        root.right = sortedListToBST(p.next);
-        return root;
+        //3.最后一种情况，也就是左右孩子都不为空，返回最小深度+1即可
+        return Math.min(m1, m2) + 1;
     }
 }
