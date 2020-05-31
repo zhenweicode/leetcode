4adaa92505d6c481d3670c0cabf856c8d9f7ad6a
xiezhenwei <xiezhenwei@meituan.com>
2020-05-31 19:23:01 +0800

    init

--- test/src/main/java/Solution.java
+++ test/src/main/java/Solution.java
@@ -1,16 +1,20 @@
 import java.util.*;
 
 class Solution {
-    public int sumNumbers(TreeNode root) {
-        return helper(root, 0);
-    }
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int len = gas.length;
+        int spare = 0;
+        int minSpare = Integer.MAX_VALUE;
+        int minIndex = 0;
 
-    public int helper(TreeNode root, int i) {
-        if (root == null) return 0;
-        int temp = i * 10 + root.val;
-        if (root.left == null && root.right == null)
-            return temp;
+        for (int i = 0; i < len; i++) {
+            spare += gas[i] - cost[i];
+            if (spare < minSpare) {
+                minSpare = spare;
+                minIndex = i;
+            }
+        }
 
-        return helper(root.left, temp) + helper(root.right, temp);
+        return spare < 0 ? -1 : (minIndex + 1) % len;
     }
 }
