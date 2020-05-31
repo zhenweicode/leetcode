5f78aff3aada414bec4a79f25bcf6cdbff16c842
xiezhenwei <xiezhenwei@meituan.com>
2020-05-31 21:06:32 +0800

    init

--- test/src/main/java/Solution.java
+++ test/src/main/java/Solution.java
@@ -1,19 +1,25 @@
 import java.util.*;
 
 public class Solution {
-    public ListNode FindLoopPort(ListNode head) {
+    public ListNode detectCycle(ListNode head) {
         if (head == null || head.next == null) {
             return null;
         }
         ListNode fast = head;
         ListNode slow = head;
+        boolean hasCycle = false;
         while (fast.next != null && fast.next.next != null) {
             slow = slow.next; // slow指针走一步
             fast = fast.next.next; // fast指针走两步
             if (slow == fast) { // 一旦两个指针相遇，说明链表是有环的
+                hasCycle = true;
                 break;
             }
         }
+        if (!hasCycle) {
+            return null;
+        }
+
         slow = head;
         while (slow != fast) {
             slow = slow.next; // slow指针走一步
