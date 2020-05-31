a31836201f02d6aa0f98a62b1d72f08d24c3727e
xiezhenwei <xiezhenwei@meituan.com>
2020-06-01 00:14:47 +0800

    init

--- test/src/main/java/Solution.java
+++ test/src/main/java/Solution.java
@@ -1,15 +1,24 @@
 import java.util.*;
 
 public class Solution {
-    public void rotate(int[] nums, int k) {
-        int temp, previous;
-        for (int i = 0; i < k; i++) {
-            previous = nums[nums.length - 1];
-            for (int j = 0; j < nums.length; j++) {
-                temp = nums[j];
-                nums[j] = previous;
-                previous = temp;
-            }
+    public int rob(TreeNode root) {
+        HashMap<TreeNode, Integer> memo = new HashMap<>();
+        return robInternal(root, memo);
+    }
+
+    private int robInternal(TreeNode root, HashMap<TreeNode, Integer> memo) {
+        if (root == null) return 0;
+        if (memo.containsKey(root)) return memo.get(root);
+        int money = root.val;
+
+        if (root.left != null) {
+            money += (robInternal(root.left.left, memo) + robInternal(root.left.right, memo));
         }
+        if (root.right != null) {
+            money += (robInternal(root.right.left, memo) + robInternal(root.right.right, memo));
+        }
+        int result = Math.max(money, robInternal(root.left, memo) + robInternal(root.right, memo));
+        memo.put(root, result);
+        return result;
     }
 }
