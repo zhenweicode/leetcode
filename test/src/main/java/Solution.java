41fa27c698a643b84b16f0c1420683244a970a51
xiezhenwei <xiezhenwei@meituan.com>
2020-05-31 15:52:57 +0800

    init

--- test/src/main/java/Solution.java
+++ test/src/main/java/Solution.java
@@ -1,43 +1,26 @@
 import java.util.*;
 
 class Solution {
-    public List<TreeNode> generateTrees(int n) {
-        if (n == 0) {
-            return new ArrayList<>();
+    public boolean isValidBST(TreeNode head) {
+        if (head == null) {
+            return true;
         }
-        return getAns(1, n);
-    }
-
-    private List<TreeNode> getAns(int start, int end) {
-        List<TreeNode> ans = new ArrayList<>();
-        //此时没有数字，将 null 加入结果中
-        if (start > end) {
-            ans.add(null);
-            return ans;
-        }
-        //只有一个数字，当前数字作为一棵树加入结果中
-        if (start == end) {
-            TreeNode tree = new TreeNode(start);
-            ans.add(tree);
-            return ans;
-        }
-        //尝试每个数字作为根节点
-        for (int i = start; i <= end; i++) {
-            //得到所有可能的左子树
-            List<TreeNode> leftTrees = getAns(start, i - 1);
-            //得到所有可能的右子树
-            List<TreeNode> rightTrees = getAns(i + 1, end);
-            //左子树右子树两两组合
-            for (TreeNode leftTree : leftTrees) {
-                for (TreeNode rightTree : rightTrees) {
-                    TreeNode root = new TreeNode(i);
-                    root.left = leftTree;
-                    root.right = rightTree;
-                    //加入到最终结果中
-                    ans.add(root);
+        TreeNode p = head;
+        Stack<TreeNode> stack = new Stack<>();
+        int temp = Integer.MIN_VALUE;
+        while (!stack.isEmpty() || p != null) {
+            if (p != null) {
+                stack.push(p);
+                p = p.left;
+            } else {
+                p = stack.pop();
+                if (p.val <= temp) {
+                    return false;
                 }
+                temp = p.val;
+                p = p.right;
             }
         }
-        return ans;
+        return true;
     }
 }
