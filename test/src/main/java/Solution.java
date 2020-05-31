54592987c73114e5def7fa561875bf4780a3462e
xiezhenwei <xiezhenwei@meituan.com>
2020-05-31 19:09:17 +0800

    init

--- test/src/main/java/Solution.java
+++ test/src/main/java/Solution.java
@@ -1,29 +1,16 @@
 import java.util.*;
 
 class Solution {
-    private int max = Integer.MIN_VALUE;
-
-    public int maxPathSum(TreeNode root) {
-        if (root == null) {
-            return 0;
-        }
-
-        dfs(root);
-        return max;
+    public int sumNumbers(TreeNode root) {
+        return helper(root, 0);
     }
 
-    // 返回经过root的分支最大和， 即Math.max(root, root+left, root+right)
-    public int dfs(TreeNode root) {
-        if (root == null) {
-            return 0;
-        }
-        //计算左边分支最大值，左边分支如果为负数还不如不选择
-        int leftMax = Math.max(0, dfs(root.left));
-        //计算右边分支最大值，右边分支如果为负数还不如不选择
-        int rightMax = Math.max(0, dfs(root.right));
-        //left->root->right 作为路径与历史最大值做比较
-        max = Math.max(max, root.val + leftMax + rightMax);
-        // 返回经过root的最大分支给上游
-        return root.val + Math.max(leftMax, rightMax);
+    public int helper(TreeNode root, int i) {
+        if (root == null) return 0;
+        int temp = i * 10 + root.val;
+        if (root.left == null && root.right == null)
+            return temp;
+
+        return helper(root.left, temp) + helper(root.right, temp);
     }
 }
