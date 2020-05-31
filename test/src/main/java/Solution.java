3f523e92b19126a7051d65d2dd9d9f3a487dcbd5
xiezhenwei <xiezhenwei@meituan.com>
2020-05-31 13:59:27 +0800

    init

--- test/src/main/java/Solution.java
+++ test/src/main/java/Solution.java
@@ -1,25 +1,41 @@
 import java.util.*;
 
 class Solution {
-    public ListNode deleteDuplicates(ListNode head) {
-        ListNode dummy = new ListNode(0);
-        dummy.next = head;
-        ListNode slow = dummy, fast = head;
-        boolean repeat = false;  // 是否重复
-        while (slow.next != null) {
-            while (fast.next != null && fast.next.val == slow.next.val) {
-                fast = fast.next;
-                repeat = true;//如果有重复节点
+    public int largestRectangleArea(int[] heights) {
+        int n = heights.length;
+        int[] left = new int[n];  // 第i个元素表示i左侧第一个比他矮的下标
+        int[] right = new int[n];  // 第i个元素表示i右侧第一个比他矮的下标
+
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < n; i++) {
+            // 丢掉的都是比heights[i]高的，肯定不是heights[i+1]左侧的最小值
+            while (!stack.isEmpty() && heights[stack.peek()] >= heights[i]) {
+                stack.pop();
             }
-            if (repeat) {
-                slow.next = fast.next;//删掉重复节点
-                fast = fast.next;//再指针后移！！这时候slow不用动！
-                repeat = false;
-            } else {
-                slow = fast;//指针后移!slow,fast都向后移动一下
-                if (fast.next != null) fast = fast.next;
-            }
+
+            // 栈不为空，栈顶是第一个比他小的，栈为空，则设置为-1，表示没有比他矮的
+            left[i] = (stack.isEmpty() ? -1 : stack.peek());
+            stack.push(i);
         }
-        return dummy.next;
+
+        stack.clear();
+        for (int i = n - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && heights[stack.peek()] >= heights[i]) {
+                stack.pop();
+            }
+
+            // 栈不为空，栈顶是第一个比他小的，栈为空，则设置为n，表示没有比他矮的
+            // 注意这里设为n，计算面积的时候就可以(n减去某个值)，表示右侧所有的柱形都有效
+            right[i] = (stack.isEmpty() ? n : stack.peek());
+            stack.push(i);
+        }
+
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            // 计算面积是在开区间(left[i], right[i])，因此right[i] - left[i] - 1
+            // 这里也可以看出来上面设置-1和n的用意
+            ans = Math.max(ans, (right[i] - left[i] - 1) * heights[i]);
+        }
+        return ans;
     }
 }
