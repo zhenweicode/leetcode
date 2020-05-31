81eed45bae32b7b5177ba525e744494ba36653db
xiezhenwei <xiezhenwei@meituan.com>
2020-05-31 21:27:31 +0800

    init

--- test/src/main/java/Solution.java
+++ test/src/main/java/Solution.java
@@ -1,30 +1,51 @@
 import java.util.*;
 
 public class Solution {
-    public ListNode detectCycle(ListNode head) {
-        if (head == null || head.next == null) {
-            return null;
-        }
-        ListNode fast = head;
-        ListNode slow = head;
-        boolean hasCycle = false;
-        while (fast.next != null && fast.next.next != null) {
-            slow = slow.next; // slow指针走一步
-            fast = fast.next.next; // fast指针走两步
-            if (slow == fast) { // 一旦两个指针相遇，说明链表是有环的
-                hasCycle = true;
-                break;
-            }
-        }
-        if (!hasCycle) {
-            return null;
+    public void reorderList(ListNode head) {
+        if (head == null || head.next == null || head.next.next == null) {
+            return;
         }
 
-        slow = head;
-        while (slow != fast) {
-            slow = slow.next; // slow指针走一步
-            fast = fast.next; // fast指针走两步
+        // 找中点
+        ListNode mid = getMid(head);
+        ListNode newHead = mid.next;
+        mid.next = null;
+
+        //第二个链表倒置
+        newHead = reverseList(newHead);
+
+        ListNode headTemp = head; // 不改变原有的头节点
+        //链表节点依次连接
+        while (newHead != null) {
+            ListNode temp = newHead.next;
+            newHead.next = headTemp.next;
+            headTemp.next = newHead;
+            headTemp = newHead.next;
+            newHead = temp;
         }
+    }
+
+    private ListNode getMid(ListNode head) {
+        // 找中点，链表分成两个
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
         return slow;
+    }
+
+    private ListNode reverseList(ListNode head) {
+        ListNode pre = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = pre;
+            pre = curr;
+            curr = next;
+        }
+        return pre;
     }
 }
