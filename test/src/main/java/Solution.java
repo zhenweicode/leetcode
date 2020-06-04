85527fbf6d749cb3dfa150ed1c4c6ecfaa672ec7
xiezhenwei <xiezhenwei@meituan.com>
2020-06-05 00:14:37 +0800

    push

--- test/src/main/java/Solution.java
+++ test/src/main/java/Solution.java
@@ -1,67 +1,31 @@
 import java.util.*;
 
-class WordDictionary {
-    class TrieNode {
-        TrieNode[] children;
-        boolean flag;
+class Solution {
+    public int maximalSquare(char[][] matrix) {
+        if (matrix == null || matrix.length < 1 || matrix[0].length < 1) return 0;
 
-        public TrieNode() {
-            children = new TrieNode[26];
-            flag = false;
-            for (int i = 0; i < 26; i++) {
-                children[i] = null;
-            }
-        }
+        int width = matrix[0].length;
+        int maxSide = 0;
 
-    }
+        int[] dp = new int[width + 1];
+        int northwest = 0; // 西北角、左上角
 
-    TrieNode root;
-
-    public WordDictionary() {
-        root = new TrieNode();
-    }
-
-    public void addWord(String word) {
-        char[] array = word.toCharArray();
-        TrieNode cur = root;
-        for (int i = 0; i < array.length; i++) {
-            // 当前孩子是否存在
-            if (cur.children[array[i] - 'a'] == null) {
-                cur.children[array[i] - 'a'] = new TrieNode();
-            }
-            cur = cur.children[array[i] - 'a'];
-        }
-        // 当前节点代表结束
-        cur.flag = true;
-    }
-
-    // 查询是否存在，支持.匹配
-    public boolean search(String word) {
-        return searchHelp(word, root);
-    }
-
-    private boolean searchHelp(String word, TrieNode root) {
-        char[] array = word.toCharArray();
-        TrieNode cur = root;
-        for (int i = 0; i < array.length; i++) {
-            // 对于 . , 递归的判断所有不为空的孩子
-            if (array[i] == '.') {
-                for (int j = 0; j < 26; j++) {
-                    if (cur.children[j] != null) {
-                        if (searchHelp(word.substring(i + 1), cur.children[j])) {
-                            return true;
-                        }
-                    }
+        for (char[] chars : matrix) {
+            northwest = 0; // 遍历每行时，还原回辅助的原值0
+            for (int col = 0; col < width; col++) {
+                int nextNorthwest = dp[col + 1];
+                if (chars[col] == '1') {
+                    dp[col + 1] = Math.min(Math.min(dp[col], dp[col + 1]), northwest) + 1;
+                    maxSide = Math.max(maxSide, dp[col + 1]);
+                } else {
+                    dp[col + 1] = 0;
                 }
-                return false;
+                northwest = nextNorthwest;
             }
-            // 不含有当前节点
-            if (cur.children[array[i] - 'a'] == null) {
-                return false;
-            }
-            cur = cur.children[array[i] - 'a'];
         }
-        // 当前节点是否为是某个单词的结束
-        return cur.flag;
+        return maxSide * maxSide;
     }
 }
+
+
+
