1dd60bece3d8626d477b7e923362e5368373d5d2
xiezhenwei <xiezhenwei@meituan.com>
2020-06-06 22:52:06 +0800

    init

--- test/src/main/java/Solution.java
+++ test/src/main/java/Solution.java
@@ -1,56 +1,20 @@
 import java.util.*;
 
 
-public class Solution {
-
-    // row == col对角线
-    private int diagonal;
-
-    // col = size - row - 1反对角线
-    private int reverseDiagonal;
-
-    private int[] rows;
-
-    private int[] cols;
-
-    private int size;
-
-    public Solution(int n) {
-        this.size = n;
-        this.rows = new int[n];
-        this.cols = new int[n];
-    }
-
-    /**
-     * Player {player} makes a move at ({row}, {col}).
-     *
-     * @param row    The row of the board.
-     * @param col    The column of the board.
-     * @param player The player, can be either 1 or 2.
-     * @return The current winning condition, can be either:
-     * 0: No one wins.
-     * 1: Player 1 wins.
-     * 2: Player 2 wins.
-     */
-    public int move(int row, int col, int player) {
-        int toAdd = player == 1 ? 1 : -1;
-
-        rows[row] += toAdd;
-        cols[col] += toAdd;
-
-        if (row == col) {
-            diagonal += toAdd;
+class Solution {
+    public int compareVersion(String version1, String version2) {
+        int i = 0, j = 0;
+        while (i < version1.length() || j < version2.length()) {
+            int x = i, y = j;
+            while (x < version1.length() && version1.charAt(x) != '.') x++;
+            while (y < version2.length() && version2.charAt(y) != '.') y++;
+            int a = x == i ? 0 : Integer.parseInt(version1.substring(i, x));
+            int b = y == j ? 0 : Integer.parseInt(version2.substring(j, y));
+            if (a < b) return -1;
+            if (a > b) return 1;
+            i = x + 1;
+            j = y + 1;
         }
-
-        if (col == size - row - 1) {
-            reverseDiagonal += toAdd;
-        }
-
-        if (Math.abs(rows[row]) == size || Math.abs(cols[col]) == size
-                || Math.abs(diagonal) == size || Math.abs(reverseDiagonal) == size) {
-            return player;
-        }
-
         return 0;
     }
 }
