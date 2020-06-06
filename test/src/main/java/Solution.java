97cf819d8098306c220af6aadffccaf9eb1c74e3
xiezhenwei <xiezhenwei@meituan.com>
2020-06-07 00:27:00 +0800

    init

--- test/src/main/java/Solution.java
+++ test/src/main/java/Solution.java
@@ -2,19 +2,32 @@ import java.util.*;
 
 
 class Solution {
-    public void moveZeroes(int[] nums) {
-        if (nums == null) {
-            return;
-        }
-        //两个指针i和j
-        int j = 0;
-        for (int i = 0; i < nums.length; i++) {
-            //当前元素!=0，就把其交换到左边，等于0的交换到右边
-            if (nums[i] != 0) {
-                int tmp = nums[i];
-                nums[i] = nums[j];
-                nums[j++] = tmp;
+    public String removeKdigits(String num, int k) {
+        LinkedList<Character> stack = new LinkedList<>();
+        for (char digit : num.toCharArray()) {
+            while (stack.size() > 0 && k > 0 && stack.peekLast() > digit) {
+                stack.removeLast();
+                k -= 1;
             }
+            stack.addLast(digit);
         }
+
+        // 保持降序的删除完了，删除最后几位最大的
+        for (int i = 0; i < k; i++) {
+            stack.removeLast();
+        }
+
+        // build the final string, while removing the leading zeros.
+        StringBuilder ret = new StringBuilder();
+        boolean leadingZero = true;
+        for (char digit : stack) {  // 添加从尾部，从头遍历，因此是最高位
+            if (leadingZero && digit == '0') continue;
+            leadingZero = false;
+            ret.append(digit);
+        }
+
+        /* return the final string  */
+        if (ret.length() == 0) return "0";
+        return ret.toString();
     }
 }
