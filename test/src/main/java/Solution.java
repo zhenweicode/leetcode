6d11e2f9740ea2d0b6cf6c244adba6daae05918c
xiezhenwei <xiezhenwei@meituan.com>
2020-06-06 14:19:52 +0800

    init

--- test/src/main/java/Solution.java
+++ test/src/main/java/Solution.java
@@ -1,24 +1,57 @@
 import java.util.*;
 
 class Solution {
-    public int shortestWordDistance(String[] words, String word1, String word2) {//相等时值更新一个。。。
-        int idx1 = -1, idx2 = -1, res = Integer.MAX_VALUE;
-        for (int i = 0; i < words.length; i++) {
-            if (words[i].equals(word1)) {
-                idx1 = i;
-                if (idx2 >= 0) res = Math.min(res, Math.abs(idx2 - idx1));
+    public void setZeroes(int[][] matrix) {
+        boolean rowFlag = false;
+        //判断首行
+        for (int i = 0; i < matrix[0].length; i++) {
+            if (matrix[0][i] == 0) {
+                rowFlag = true;
+                break;
             }
-            if (words[i].equals(word2)) {
-                idx2 = i;
-                if (idx1 >= 0 && idx1 != idx2) {
-                    res = Math.min(res, Math.abs(idx2 - idx1));
+        }
+
+        boolean colFlag = false;
+        for (int i = 0; i < matrix.length; i++) {
+            if (matrix[i][0] == 0) {
+                colFlag = true;
+                break;
+            }
+        }
+
+        for (int i = 1; i < matrix.length; i++) {
+            for (int j = 1; j < matrix[0].length; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
             }
         }
 
-        return res;
+        for (int i = 1; i < matrix[0].length; i++) {
+            if (matrix[0][i] == 0) {
+                for (int j = 0; j < matrix.length; j++) {
+                    matrix[j][i] = 0;
+                }
+            }
+        }
+
+        for (int i = 1; i < matrix.length; i++) {
+            if (matrix[i][0] == 0) {
+                for (int j = 0; j < matrix[0].length; j++) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        if (rowFlag) {
+            for (int i = 0; i < matrix[0].length; i++) {
+                matrix[0][i] = 0;
+            }
+        }
+        if (colFlag) {
+            for (int i = 0; i < matrix.length; i++) {
+                matrix[i][0] = 0;
+            }
+        }
     }
 }
-
-
-
