7a0bb38bd9d5dd902fbbb3e8b7c2fdecf0fbf684
xiezhenwei <xiezhenwei@meituan.com>
2020-06-06 23:18:02 +0800

    init

--- test/src/main/java/Solution.java
+++ test/src/main/java/Solution.java
@@ -2,25 +2,19 @@ import java.util.*;
 
 
 class Solution {
-    public String reverseWords(String s) {
-        if (s == null || s.length() == 0) {
-            return s;
+    public void moveZeroes(int[] nums) {
+        if (nums == null) {
+            return;
         }
-
-        List<String> result = new ArrayList<>();
-        for (String s1 : s.split("\\s+")) {
-            result.add(reverseWord(s1));
+        //两个指针i和j
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
+            //当前元素!=0，就把其交换到左边，等于0的交换到右边
+            if (nums[i] != 0) {
+                int tmp = nums[i];
+                nums[i] = nums[j];
+                nums[j++] = tmp;
+            }
         }
-        return String.join(" ", result);
-    }
-
-    private String reverseWord(String s) {
-        StringBuilder b = new StringBuilder();
-        char[] c = s.toCharArray();
-        for (int i = c.length - 1; i >= 0; i--) {
-            b.append(c[i]);
-        }
-
-        return b.toString();
     }
 }
