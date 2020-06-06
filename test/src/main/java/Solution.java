b344e1ea2465ecc4fa5780e127cea03f9d208cc6
xiezhenwei <xiezhenwei@meituan.com>
2020-06-06 15:15:03 +0800

    init

--- test/src/main/java/Solution.java
+++ test/src/main/java/Solution.java
@@ -1,25 +1,21 @@
 class Solution {
-    public Node connect(Node root) {
-        if (root == null) return root;
+    private HashMap<Node, Node> visited = new HashMap<>();
 
-        Node head = root;
-        while(head != null) {
-            Node dummy = new Node();
-            Node cur = dummy;
-            while(head != null) {
-                if (head.left != null) {
-                    cur.next = head.left;
-                    cur = cur.next;
-                }
-                if (head.right != null) {
-                    cur.next = head.right;
-                    cur = cur.next;
-                }
-                head = head.next;
-            }
-            head = dummy.next;  // 下一层最左节点
+    public Node cloneGraph(Node node) {
+        if (node == null) {
+            return node;
         }
 
-        return root;
+        if (visited.containsKey(node)) {
+            return visited.get(node);
+        }
+
+        Node cloneNode = new Node(node.val, new ArrayList());
+        visited.put(node, cloneNode);
+
+        for (Node neighbor : node.neighbors) {
+            cloneNode.neighbors.add(cloneGraph(neighbor));
+        }
+        return cloneNode;
     }
 }
