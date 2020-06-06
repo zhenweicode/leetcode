a1feb5ae16eef3f2e534f363e9167da13cef8056
xiezhenwei <xiezhenwei@meituan.com>
2020-06-06 11:03:54 +0800

    push

--- test/src/main/java/Solution.java
+++ test/src/main/java/Solution.java
@@ -1,29 +1,77 @@
 import java.util.*;
 
-class Solution {
-    public int maximalSquare(char[][] matrix) {
-        if (matrix == null || matrix.length < 1 || matrix[0].length < 1) return 0;
+class MyStack {
+    private Queue<Integer> queue1 = null;
+    private Queue<Integer> queue2 = null;
+    private int flag = 0;
 
-        int width = matrix[0].length;
-        int maxSide = 0;
+    public MyStack() {
+        queue1 = new LinkedList<>();
+        queue2 = new LinkedList<>();
+    }
 
-        int[] dp = new int[width + 1];
-        int northwest = 0; // 西北角、左上角
-
-        for (char[] chars : matrix) {
-            northwest = 0; // 遍历每行时，还原回辅助的原值0
-            for (int col = 0; col < width; col++) {
-                int nextNorthwest = dp[col + 1];
-                if (chars[col] == '1') {
-                    dp[col + 1] = Math.min(Math.min(dp[col], dp[col + 1]), northwest) + 1;
-                    maxSide = Math.max(maxSide, dp[col + 1]);
-                } else {
-                    dp[col + 1] = 0;
-                }
-                northwest = nextNorthwest;
-            }
+    public void push(int x) {
+        if (flag == 0) {
+            queue1.offer(x);
+        } else {
+            queue2.offer(x);
         }
-        return maxSide * maxSide;
+    }
+
+    public int pop() {
+        int num = 0;
+        if (flag == 0 && !queue1.isEmpty()) {
+            num = pop(queue1, queue2);
+            flag = 1;
+        } else if (flag == 1 && !queue2.isEmpty()) {
+            num = pop(queue2, queue1);
+            flag = 0;
+        } else {
+            throw new NullPointerException("stack is empty");
+        }
+
+        return num;
+    }
+
+    private int pop(Queue<Integer> q1, Queue<Integer> q2) {
+        if (q1.size() == 1) {
+            return q1.poll();
+        }
+
+        while (q1.size() > 1) {
+            q2.offer(q1.poll());
+        }
+
+        return q1.poll();
+    }
+
+    public int top() {
+        int num = 0;
+        if (flag == 0 && !queue1.isEmpty()) {
+            num = top(queue1, queue2);
+        } else if (flag == 1 && !queue2.isEmpty()) {
+            num = top(queue2, queue1);
+        } else {
+            throw new NullPointerException("stack is empty");
+        }
+
+        return num;
+    }
+
+    private int top(Queue<Integer> q1, Queue<Integer> q2) {
+        if (q1.size() == 1) {
+            return q1.peek();
+        }
+
+        while (q1.size() > 1) {
+            q2.offer(q1.poll());
+        }
+
+        return q1.peek();
+    }
+
+    public boolean empty() {
+        return queue1.isEmpty() && queue2.isEmpty();
     }
 }
 
