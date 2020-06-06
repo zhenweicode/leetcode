2820d9d825354b9cf7d21dea3a1ee50b092b726c
xiezhenwei <xiezhenwei@meituan.com>
2020-06-06 15:07:20 +0800

    init

--- test/src/main/java/Solution.java
+++ test/src/main/java/Solution.java
@@ -1,22 +1,23 @@
-import java.util.*;
-
 class Solution {
     public Node connect(Node root) {
         if (root == null) return root;
 
-        Node leftmost = root;
-        while (leftmost.left != null) {
-            Node head = leftmost;
-            while (head != null) {
-                head.left.next = head.right;
-                if (head.next != null) {
-                    head.right.next = head.next.left;
+        Node head = root;
+        while(head != null) {
+            Node dummy = new Node();
+            Node cur = dummy;
+            while(head != null) {
+                if (head.left != null) {
+                    cur.next = head.left;
+                    cur = cur.next;
                 }
-
+                if (head.right != null) {
+                    cur.next = head.right;
+                    cur = cur.next;
+                }
                 head = head.next;
             }
-
-            leftmost = leftmost.left;
+            head = dummy.next;  // 下一层最左节点
         }
 
         return root;
