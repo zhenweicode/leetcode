ec4234f4e92a91148ced82c33221346eb34f0217
xiezhenwei <xiezhenwei@meituan.com>
2020-06-06 16:10:27 +0800

    init

--- test/src/main/java/Solution.java
+++ test/src/main/java/Solution.java
@@ -1,96 +1,56 @@
 import java.util.*;
 
-class Solution {
-    public List<String> findWords(char[][] board, String[] words) {
-        //构建字典树
-        WordTrie myTrie = new WordTrie();
-        TrieNode root = myTrie.root;
-        //插入数据
-        for (String word : words) {
-            myTrie.insert(word);
+public class Codec {
+
+    public String serialize(TreeNode root) {
+        if (root == null) {
+            return "";
         }
 
-        //构建结果集容器
-        List<String> result = new LinkedList<>();
-        //矩阵行数
-        int m = board.length;
-        //矩阵列数
-        int n = board[0].length;
-        //存储该节点是否访问
-        boolean[][] visited = new boolean[m][n];
-        //遍历整个二维数组
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                find(board, visited, i, j, m, n, result, root);
-            }
-        }
-        return result;
+        String str = root.val + ",";
+        str += serialize(root.left);
+        str += serialize(root.right);
+        return str;
     }
 
-    private void find(char[][] board, boolean[][] visited, int i, int j, int m, int n, List<String> result, TrieNode cur) {
-        //边界判断以及是否已经访问判断
-        if (i < 0 || i >= m || j < 0 || j >= n || visited[i][j])
-            return;
-        //获取子节点状态，判断其是否有子节点
-        cur = cur.child[board[i][j] - 'a'];
-        if (cur == null) {
-            return;
+    public TreeNode deserialize(String data) {
+        if (data == null || data.length() == 0) {
+            return null;
         }
-        //修改节点状态，防止重复访问
-        visited[i][j] = true;
-        //找到单词加入
-        if (cur.isEnd) {
-            result.add(cur.val);
-            //找到单词后，修改字典树内叶子节点状态为false，防止出现重复单词
-            cur.isEnd = false;
+        String[] dataStr = data.split(",");
+        int[] pre = new int[dataStr.length];
+        int[] in = new int[dataStr.length];
+        for (int i = 0; i < dataStr.length; i++) {
+            pre[i] = Integer.valueOf(dataStr[i]);
+            in[i] = Integer.valueOf(dataStr[i]);
         }
-        find(board, visited, i + 1, j, m, n, result, cur);
-        find(board, visited, i - 1, j, m, n, result, cur);
-        find(board, visited, i, j + 1, m, n, result, cur);
-        find(board, visited, i, j - 1, m, n, result, cur);
-        //最后修改节点状态为未访问状态
-        visited[i][j] = false;
+
+        Arrays.sort(in);
+        return buildTree(pre, in);
     }
 
-
-    /**
-     * 字典树
-     */
-    class WordTrie {
-        //创建根节点
-        TrieNode root = new TrieNode();
-
-        void insert(String s) {
-            TrieNode cur = root;
-            for (char c : s.toCharArray()) {
-                //判断是否存在该字符的节点，不存在则创建
-                if (cur.child[c - 'a'] == null) {
-                    cur.child[c - 'a'] = new TrieNode();
-                    cur = cur.child[c - 'a'];
-                } else
-                    cur = cur.child[c - 'a'];
-            }
-            //遍历结束后，修改叶子节点的状态，并存储字符串
-            cur.isEnd = true;
-            cur.val = s;
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        if (preorder == null || inorder == null) {
+            return null;
         }
+
+        Map<Integer, Integer> indexMap = new HashMap<Integer, Integer>(preorder.length);
+        for (int i = 0; i < inorder.length; i++) {
+            indexMap.put(inorder[i], i);
+        }
+
+        return buildTree(preorder, 0, preorder.length - 1, inorder, 0, inorder.length - 1, indexMap);
     }
 
-    /**
-     * 字典树节点
-     */
-    class TrieNode {
-        /**
-         * 存储最后节点的字符串
-         */
-        String val;
-        /**
-         * 根据字符排序，[a,b,c,……,z]
-         */
-        TrieNode[] child = new TrieNode[26];
-        /**
-         * 是否是最后叶子节点
-         */
-        boolean isEnd = false;
+    private TreeNode buildTree(int[] p, int pi, int pj, int[] n, int ni, int nj, Map<Integer, Integer> indexMap) {
+        if (pi > pj) {
+            return null;
+        }
+
+        TreeNode head = new TreeNode(p[pi]);
+        int index = indexMap.get(p[pi]);
+        head.left = buildTree(p, pi + 1, pi + index - ni, n, ni, index - 1, indexMap);  // index - ni表示左子树长度，pi + index - ni表示前序中左子树最大下标
+        head.right = buildTree(p, pi + index - ni + 1, pj, n, index + 1, nj, indexMap);
+        return head;
     }
 }
