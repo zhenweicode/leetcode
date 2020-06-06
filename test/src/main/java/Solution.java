b50a0e514d6852b9779b62365dbcc5bc2e956160
xiezhenwei <xiezhenwei@meituan.com>
2020-06-06 14:54:31 +0800

    init

--- test/src/main/java/Solution.java
+++ test/src/main/java/Solution.java
@@ -1,29 +1,24 @@
 import java.util.*;
 
 class Solution {
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        Stack<Integer> stack1 = new Stack<>();
-        Stack<Integer> stack2 = new Stack<>();
-        while (l1 != null) {
-            stack1.push(l1.val);
-            l1 = l1.next;
-        }
-        while (l2 != null) {
-            stack2.push(l2.val);
-            l2 = l2.next;
+    public Node connect(Node root) {
+        if (root == null) return root;
+
+        Node leftmost = root;
+        while (leftmost.left != null) {
+            Node head = leftmost;
+            while (head != null) {
+                head.left.next = head.right;
+                if (head.next != null) {
+                    head.right.next = head.next.left;
+                }
+
+                head = head.next;
+            }
+
+            leftmost = leftmost.left;
         }
 
-        int carry = 0;
-        ListNode head = null;
-        while (!stack1.isEmpty() || !stack2.isEmpty() || carry > 0) {
-            int sum = carry;
-            sum += stack1.isEmpty() ? 0 : stack1.pop();
-            sum += stack2.isEmpty() ? 0 : stack2.pop();
-            ListNode node = new ListNode(sum % 10);
-            node.next = head;
-            head = node;
-            carry = sum / 10;
-        }
-        return head;
+        return root;
     }
 }
