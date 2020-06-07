2e6559590fd1d5f9ff6d5ec4b94398340bae1b86
xiezhenwei <xiezhenwei@meituan.com>
2020-06-07 12:43:52 +0800

    init

--- test/src/main/java/Solution.java
+++ test/src/main/java/Solution.java
@@ -2,48 +2,70 @@ import java.util.*;
 
 
 class Solution {
-    public TreeNode deleteNode(TreeNode root, int key) {
-        if (root == null) {
-            return null;
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        int end = wordList.indexOf(endWord);
+        if (end == -1) {
+            return 0;
         }
-        if (key < root.val) {
-            // 待删除节点在左子树中
-            root.left = deleteNode(root.left, key);
-            return root;
-        } else if (key > root.val) {
-            // 待删除节点在右子树中
-            root.right = deleteNode(root.right, key);
-            return root;
-        } else {
-            // key == root.val，root 为待删除节点
-            if (root.left == null) {
-                // 返回右子树作为新的根
-                return root.right;
-            } else if (root.right == null) {
-                // 返回左子树作为新的根
-                return root.left;
-            } else {
-                // 左右子树都存在，返回后继节点（右子树最左叶子）作为新的根
-                TreeNode successor = min(root.right);
-                successor.right = deleteMin(root.right);
-                successor.left = root.left;
-                return successor;
+        wordList.add(beginWord);
+
+        // 从两端BFS遍历要用的队列
+        Queue<String> queue1 = new LinkedList<>();
+        Queue<String> queue2 = new LinkedList<>();
+        // 两端已经遍历过的节点
+        Set<String> visited1 = new HashSet<>();
+        Set<String> visited2 = new HashSet<>();
+        queue1.offer(beginWord);
+        queue2.offer(endWord);
+        visited1.add(beginWord);
+        visited2.add(endWord);
+
+        int level = 0;
+        Set<String> allWordSet = new HashSet<>(wordList);
+
+        while (!queue1.isEmpty() && !queue2.isEmpty()) {
+            level++;
+            if (queue1.size() > queue2.size()) {
+                Queue<String> tmp = queue1;
+                queue1 = queue2;
+                queue2 = tmp;
+                Set<String> t = visited1;
+                visited1 = visited2;
+                visited2 = t;
+            }
+
+            int size1 = queue1.size();
+            while (size1 > 0) {
+                size1--;
+                String s = queue1.poll();
+                char[] chars = s.toCharArray();
+                for (int j = 0; j < s.length(); j++) {
+                    // 保存第j位的原始字符
+                    char c0 = chars[j];
+                    for (char c = 'a'; c <= 'z'; ++c) {
+                        chars[j] = c;
+                        String newString = new String(chars);
+                        // 已经访问过了，跳过
+                        if (visited1.contains(newString)) {
+                            continue;
+                        }
+
+                        // 两端遍历相遇，结束遍历，返回level
+                        if (visited2.contains(newString)) {
+                            return level + 1;
+                        }
+
+                        // 如果单词在列表中存在，将其添加到队列，并标记为已访问
+                        if (allWordSet.contains(newString)) {
+                            queue1.offer(newString);
+                            visited1.add(newString);
+                        }
+                    }
+                    // 恢复第j位的原始字符
+                    chars[j] = c0;
+                }
             }
         }
-    }
-
-    private TreeNode min(TreeNode node) {
-        if (node.left == null) {
-            return node;
-        }
-        return min(node.left);
-    }
-
-    private TreeNode deleteMin(TreeNode node) {
-        if (node.left == null) {
-            return node.right;
-        }
-        node.left = deleteMin(node.left);
-        return node;
+        return 0;
     }
 }
