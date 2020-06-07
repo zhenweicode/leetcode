571404073e1493e90465c6093b693b44be1e8c28
xiezhenwei <xiezhenwei@meituan.com>
2020-06-07 17:48:04 +0800

    init

--- test/src/main/java/Solution.java
+++ test/src/main/java/Solution.java
@@ -1,51 +1,37 @@
 import java.util.*;
 
 
-public class Solution {
-    public int lengthOfLIS(int[] nums) {
-        int len = nums.length;
-        if (len <= 1) {
-            return len;
-        }
+class Solution {
+    private int[] array;
+    private int[] original;
 
-        // tail 数组的定义：长度为 i + 1 的最长上升子序列的末尾最小是几
-        int[] tail = new int[len];
-        // 遍历第 1 个数，直接放在有序数组 tail 的开头
-        tail[0] = nums[0];
-        // end 表示有序数组 tail 的最后一个已经赋值元素的索引
-        int end = 0;
+    private Random rand = new Random();
 
-        for (int i = 1; i < len; i++) {
-            // 【逻辑 1】比 tail 数组实际有效的末尾的那个元素还大
-            if (nums[i] > tail[end]) {
-                // 直接添加在那个元素的后面，所以 end 先加 1
-                end++;
-                tail[end] = nums[i];
-            } else {
-                // 使用二分查找法，在有序数组 tail 中
-                // 找到第 1 个大于等于 nums[i] 的元素，尝试让那个元素更小
-                int left = 0;
-                int right = end;
-                while (left < right) {
-                    // 选左中位数不是偶然，而是有原因的
-                    int mid = left + (right - left) / 2;
-                    if (tail[mid] < nums[i]) {
-                        // 中位数肯定不是要找的数，把它写在分支的前面
-                        left = mid + 1;
-                    } else {
-                        right = mid;
-                    }
-                }
-                // 走到这里是因为 【逻辑 1】 的反面，因此一定能找到第 1 个大于等于 nums[i] 的元素
-                // 因此，无需再单独判断
-                tail[left] = nums[i];
-            }
-            // 调试方法
-            // printArray(nums[i], tail);
+    private int randRange(int min, int max) {
+        return rand.nextInt(max - min) + min;
+    }
+
+    private void swapAt(int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    public Solution(int[] nums) {
+        array = nums;
+        original = nums.clone();
+    }
+
+    public int[] reset() {
+        array = original;
+        original = original.clone();
+        return original;
+    }
+
+    public int[] shuffle() {
+        for (int i = 0; i < array.length; i++) {
+            swapAt(i, randRange(i, array.length));
         }
-        // 此时 end 是有序数组 tail 最后一个元素的索引
-        // 题目要求返回的是长度，因此 +1 后返回
-        end++;
-        return end;
+        return array;
     }
 }
