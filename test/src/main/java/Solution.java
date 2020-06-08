666cd911064e81fb09cfad4ec79bac4f5a7aa116
xiezhenwei <xiezhenwei@meituan.com>
2020-06-09 00:30:09 +0800

    init

--- test/src/main/java/Solution.java
+++ test/src/main/java/Solution.java
@@ -1,44 +1,32 @@
 import java.util.*;
 
-class RandomizedSet {
-    private Map<Integer, Integer> dict;
-    private List<Integer> list;
-    private Random rand = new Random();
+class Solution {
+    private int max = 0;
 
-    public RandomizedSet() {
-        dict = new HashMap<>();
-        list = new ArrayList<>();
+    public int longestUnivaluePath(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        dfs(root);
+        return max;
     }
 
-    public boolean insert(int val) {
-        if (dict.containsKey(val)) return false;
+    private int dfs(TreeNode root) {
+        if (root.left == null && root.right == null) {
+            return 0;
+        }
 
-        dict.put(val, list.size());
-        list.add(list.size(), val);  // 不会每次触发扩容
-        return true;
-    }
-
-    /**
-     * Removes a value from the set. Returns true if the set contained the specified element.
-     */
-    public boolean remove(int val) {
-        if (!dict.containsKey(val)) return false;
-
-        // move the last element to the place idx of the element to delete
-        int lastElement = list.get(list.size() - 1);
-        int idx = dict.get(val);
-        list.set(idx, lastElement);
-        dict.put(lastElement, idx);
-        // delete the last element
-        list.remove(list.size() - 1);
-        dict.remove(val);
-        return true;
-    }
-
-    /**
-     * Get a random element from the set.
-     */
-    public int getRandom() {
-        return list.get(rand.nextInt(list.size()));
+        int leftSize = root.left != null ? dfs(root.left) + 1 : 0;
+        int rightSize = root.right != null ? dfs(root.right) + 1 : 0;
+        if (leftSize > 0 && root.left.val != root.val) {
+            // 唯一的区别在这里，按照上题思路求出了左边边长后， 如果当前节点和左孩子节点不同值，就把边长重新赋值为0。
+            leftSize = 0;
+        }
+        if (rightSize > 0 && root.right.val != root.val) {
+            // 同上。
+            rightSize = 0;
+        }
+        max = Math.max(max, leftSize + rightSize);
+        return Math.max(leftSize, rightSize);
     }
 }
